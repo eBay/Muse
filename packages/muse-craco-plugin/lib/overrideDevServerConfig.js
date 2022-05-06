e864c0a2c4a1d46cea85699926993ea21b9da56b
Nate Wang <pwang7@ebay.com>
2022-05-06 18:42:43 +0800

    Created muse dev middleware.

--- packages/muse-craco-plugin/lib/overrideDevServerConfig.js
+++ packages/muse-craco-plugin/lib/overrideDevServerConfig.js
@@ -1,3 +1,18 @@
+const museDevMiddleware = require('./museDevMiddleware');
 module.exports = ({ devServerConfig, cracoConfig, pluginOptions, context: { env, paths, proxy, allowedHost } }) => {
+  const oldSetup = devServerConfig.setupMiddlewares || devServerConfig.onBeforeSetupMiddleware || (() => null);
+  if (devServerConfig.setupMiddlewares) {
+    devServerConfig.setupMiddlewares = (m, devServer) => {
+      // webpack-dev-server v4.7.0+
+      // seems can't be used together with onBeforeSetupMiddleware
+      oldSetup(m, devServer);
+      devServer.app.use('/_muse_api/*', museDevMiddleware);
+    };
+  } else {
+    devServerConfig.onBeforeSetupMiddleware = (devServer) => {
+      oldSetup(devServer);
+      devServer.app.use('/_muse_api/*', museDevMiddleware);
+    };
+  }
   return devServerConfig;
 };
