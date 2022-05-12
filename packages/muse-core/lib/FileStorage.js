80a5f6fb5849df13a2a65498d58c9a14d908fb5c
Nate Wang <pwang7@ebay.com>
2022-05-12 13:47:37 +0800

    Storage api in progress.

---
+++ packages/muse-core/lib/FileStorage.js
@@ -0,0 +1,44 @@
+const path = require('path');
+// const os = require('os');
+const fs = require('fs-extra');
+
+module.exports = {
+  init(options) {
+    if (!options?.location) throw new Error('No location specified for FileStorage.');
+    this.location = options.location;
+  },
+
+  mapPath(p) {
+    const absPath = path.resolve(this.location, p.replace());
+    if (!p.startsWith(this.location)) {
+      throw new Error('Can not access path out of ' + this.location);
+    }
+    return absPath;
+  },
+
+  // value: Buffer
+  async set(path, value) {
+    const absPath = this.mapPath(path);
+    await fs.ensureDir(path.dirname(absPath));
+    await fs.promises.writeFile(absPath, value);
+  },
+
+  async get(path) {
+    const absPath = this.mapPath(path);
+    if (!fs.existsSync(absPath)) return null;
+    await fs.ensureDir(path.dirname(absPath));
+    return await fs.promises.readFile(absPath);
+  },
+
+  async exist(path) {
+    return fs.existsSync(this.mapPath(path));
+  },
+
+  // list items in a container
+  async list(path) {
+    const absPath = this.mapPath(path);
+  },
+
+  // number of items in a container
+  async count(path) {},
+};
--- packages/muse-core/lib/config.js
+++ packages/muse-core/lib/config.js
@@ -1,3 +1,25 @@
-const config = {};
+const jsYaml = require('js-yaml');
+const path = require('path');
+const fs = require('fs-extra');
+const os = require('os');
 
-module.exports = config;
+let config = null;
+module.exports = () => {
+  if (config) return config;
+
+  // find muse config file by locations:
+  //   1. cwd
+  //   2. homedir
+
+  const configFile = [process.cwd(), os.homedir()]
+    .map((d) => path.join(d, 'muse.config.yaml'))
+    .find((f) => fs.existsSync(f));
+
+  if (!configFile) config = {};
+  else {
+    const configObj = jsYaml.load(fs.readFileSync(configFile));
+    if (configObj.provider) config = require(configObj.provider);
+    else config = configObj;
+  }
+  return config;
+};
