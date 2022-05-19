8934b8254d762037c978d9a4d14c67947a358442
Nate Wang <pwang7@ebay.com>
2022-05-19 20:42:20 +0800

    Release plugin

--- packages/muse-cli/README.md
+++ packages/muse-cli/README.md
@@ -1,2 +1,7 @@
 # muse-cli
 Muse commands to be run under a Muse plugin project.
+
+muse release
+muse deploy
+muse config
+muse export
--- packages/muse-core/lib/pm/releasePlugin.js
+++ packages/muse-core/lib/pm/releasePlugin.js
@@ -1,21 +1,21 @@
-// Release a plugin:
-// core feature:
-//   Create a release item in <registry-storage>/plugins/releases/<plugin-name>.yaml
-// other:
-// ...
+// Register a release and upload assets to the storage
+// NOTE: this method usually takes long time, don't call it as rest API but from some job.
+
 const fs = require('fs-extra');
 const yaml = require('js-yaml');
 const path = require('path');
 const { asyncInvoke, jsonByBuff, getPluginId } = require('../utils');
-const { registry } = require('../storage');
+const { assets } = require('../storage');
 const registerRelease = require('./registerRelease');
 
 module.exports = async (params) => {
   const ctx = {};
+  const { pluginName, buildDir, version, author } = params;
+  const pid = getPluginId(pluginName);
   await asyncInvoke('museCore.pm.beforeReleasePlugin', ctx, params);
   await registerRelease(params);
 
-  // await
+  await assets.uploadDir(`/p/${pid}/${version}`, buildDir, `Release plugin ${pluginName}@${version} by ${author}.`);
 
   await asyncInvoke('museCore.pm.releasePlugin', ctx, params);
 
