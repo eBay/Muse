b86c611a9a22c659bbf8aa2cc888f22408e1deaf
zhijgu <zhijgu@ebay.com>
2022-06-07 20:43:59 +0800

    muse-265
    muse-265

---
+++ examples/plugin-library/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import plugin from 'js-plugin';
+import * as charts from '@ant-design/charts';
+import * as ext from './ext';
+import * as CommonLineChart from './commonLineChart';
+
+plugin.register({
+  ...ext,
+  name: 'plugin-library', // reserve the plugin name
+});
+
+const SayHello = ({ name }: { name: string }): JSX.Element => (
+  <div>Hey {name}, say hello to TypeScript.</div>
+);
+
+export default { charts, CommonLineChart, SayHello };
