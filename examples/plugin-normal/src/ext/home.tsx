c246212956282eb28540ca7efaf7ad2f5512235a
zhijgu <zhijgu@ebay.com>
2022-06-07 20:43:59 +0800

    muse-265
    muse-265

---
+++ examples/plugin-normal/src/ext/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Line } from '@ant-design/charts';
+import pluginLibrary from 'plugin-library';
+
+const homePage = {
+  homepage: () => (
+    <div>
+        <pluginLibrary.SayHello name="Alberg"/>
+    </div> 
+),
+}
+export default homePage;
