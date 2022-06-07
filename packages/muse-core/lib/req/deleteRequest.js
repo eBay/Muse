7369f56642de96600d9f2aad079dcf1213efc0ff
Nate Wang <pwang7@ebay.com>
2022-06-07 11:41:19 +0800

    Delete request api.

---
+++ packages/muse-core/lib/req/deleteRequest.js
@@ -0,0 +1,21 @@
+const { asyncInvoke, osUsername } = require('../utils');
+const { registry } = require('../storage');
+
+module.exports = async (params) => {
+  const ctx = {};
+  const { requestId, author = osUsername, msg } = params;
+  const keyPath = `/requests/${requestId}.yaml`;
+
+  await asyncInvoke('museCore.req.beforeDeleteRequest', ctx, params);
+
+  try {
+    await asyncInvoke('museCore.req.deleteRequest', ctx, params);
+    await registry.del(keyPath, msg || `Delete request ${requestId} by ${author}.`);
+  } catch (err) {
+    ctx.error = err;
+    await asyncInvoke('museCore.req.failedDeleteRequest', ctx, params);
+    throw err;
+  }
+  await asyncInvoke('museCore.req.afterDeleteRequest', ctx, params);
+  return ctx.request;
+};
