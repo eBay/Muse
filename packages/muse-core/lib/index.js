11ffdbc06a2099e334f1e6e230dcf10dcb9de56c
Nate Wang <pwang7@ebay.com>
2022-06-13 18:20:32 +0800

    Support dotenv

--- packages/muse-core/lib/index.js
+++ packages/muse-core/lib/index.js
@@ -1,3 +1,17 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const envFile1 = path.join(process.cwd(), '.env.muse');
+const envFile2 = path.join(os.homedir(), '.env.muse');
+
+[envFile1, envFile2].some((envFile) => {
+  if (fs.existsSync(envFile)) {
+    require('dotenv').config({ path: envFile });
+    return true;
+  }
+});
+
 module.exports = {
   am: require('./am'),
   pm: require('./pm'),
