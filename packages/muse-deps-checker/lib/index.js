24eeabc8ca7b224147ca11ce725355ee3a27b4e9
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2022-06-13 11:09:55 +0800

    MJSE-260 prepare for using muse-core on deps plugin

--- packages/muse-deps-checker/lib/index.js
+++ packages/muse-deps-checker/lib/index.js
@@ -1,8 +1,17 @@
+//const muse = require('muse-core');
+
 module.exports = {
     name: 'muse-deps-checker',
     museCore: {
       pm: {
-        beforeDeployPlugin: () => console.log('checking required dependencies before deploying plugin...'),
+        beforeDeployPlugin: (ctx, params) => {
+          console.log(ctx);
+          console.log(params);
+          console.log('checking required dependencies before deploying plugin...');
+
+          //const result = await muse.storage.assets.get(decodeURIComponent(`${params.pluginName}/v${params.version}/dist/deps-manifest.json`));
+          //console.log(result);
+        },
       },
     },
   };
