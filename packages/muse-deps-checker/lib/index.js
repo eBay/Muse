b2859729cb8e9ac562bc2572d027e3600f9fb8f3
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2022-06-13 12:27:19 +0800

    MUSE-260 fix cyclic dependency

--- packages/muse-deps-checker/lib/index.js
+++ packages/muse-deps-checker/lib/index.js
@@ -1,16 +1,15 @@
-//const muse = require('muse-core');
-
 module.exports = {
     name: 'muse-deps-checker',
     museCore: {
       pm: {
-        beforeDeployPlugin: (ctx, params) => {
+        beforeDeployPlugin: async (ctx, params) => {
           console.log(ctx);
           console.log(params);
           console.log('checking required dependencies before deploying plugin...');
 
-          //const result = await muse.storage.assets.get(decodeURIComponent(`${params.pluginName}/v${params.version}/dist/deps-manifest.json`));
-          //console.log(result);
+          const muse = require('muse-core');
+          const result = await muse.storage.assets.get(decodeURIComponent(`${params.pluginName}/v${params.version}/dist/deps-manifest.json`));
+          console.log(result);
         },
       },
     },
