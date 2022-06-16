0f875403df2da64bea4daaca9433f4574ba67e49
Gao Lin <gling@ebay.com>
2022-06-16 17:55:26 +0800

    Remove token.

--- muse.config.js
+++ muse.config.js
@@ -1,13 +1,27 @@
-module.exports = () => {
-  return {
-    plugins: [[
-      '../../muse-git-storage',
+module.exports = {
+  plugins: [
+    [
+      './packages/muse-plugin-git-storage',
       {
+        pluginName: 'muse-plugin-git-storage',
+        extPoint: 'museCore.registry.storage',
         url: 'https://github.corp.ebay.com',
         organizationName: 'gling',
         projectName: 'muse-registry-sample',
-        token: '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'
-      }
-    ]]
-  };
-}
+        token: '$env.MY_TOKEN',
+      },
+    ],
+    [
+      './packages/muse-plugin-s3-storage',
+      {
+        pluginName: 'nuobject-assets-ebay',
+        extPoint: 'museCore.assets.storage',
+        basePath: '',
+        accessKey: 'muse-ffe8f21c-3718-4d62-8278-f4a4905826b1',
+        secretKey: '9127d30c-8726-4901-84c5-816058533539',
+        endPoint: 'muse.nuobject.io',
+        bucketName: 'testbucket',
+      },
+    ],
+  ],
+};
