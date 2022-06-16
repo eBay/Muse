4c35683bac181615d46c5ce75ac365f8aff69c7c
Nate Wang <pwang7@ebay.com>
2022-06-16 18:18:36 +0800

    Use local assets for now.

--- muse.config.js
+++ muse.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   plugins: [
-    [
-      './packages/muse-plugin-git-storage',
-      {
-        pluginName: 'muse-plugin-git-storage',
-        extPoint: 'museCore.registry.storage',
-        url: 'https://github.corp.ebay.com',
-        organizationName: 'gling',
-        projectName: 'muse-registry-sample',
-        token: '$env.MY_TOKEN',
-      },
-    ],
-    [
-      './packages/muse-plugin-s3-storage',
-      {
-        pluginName: 'nuobject-assets-ebay',
-        extPoint: 'museCore.assets.storage',
-        basePath: '',
-        accessKey: '$env.NUOBJECT_ACCESS_KEY',
-        secretKey: '$env.NUBOJECT_SECRET_KEY',
-        endPoint: 'muse.nuobject.io',
-        bucketName: 'testbucket',
-      },
-    ],
+    // [
+    //   './packages/muse-plugin-git-storage',
+    //   {
+    //     pluginName: 'muse-plugin-git-storage',
+    //     extPoint: 'museCore.registry.storage',
+    //     url: 'https://github.corp.ebay.com',
+    //     organizationName: 'gling',
+    //     projectName: 'muse-registry-sample',
+    //     token: '$env.MY_TOKEN',
+    //   },
+    // ],
+    // [
+    //   './packages/muse-plugin-s3-storage',
+    //   {
+    //     pluginName: 'nuobject-assets-ebay',
+    //     extPoint: 'museCore.assets.storage',
+    //     basePath: '',
+    //     accessKey: '$env.NUOBJECT_ACCESS_KEY',
+    //     secretKey: '$env.NUBOJECT_SECRET_KEY',
+    //     endPoint: 'muse.nuobject.io',
+    //     bucketName: 'testbucket',
+    //   },
+    // ],
   ],
 };
