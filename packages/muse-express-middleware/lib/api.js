d80a720335a9f95de478528d4560fc080d2acba3
Nate Wang <pwang7@ebay.com>
2022-06-25 15:01:09 +0800

    Added api middleware.

--- packages/muse-express-middleware/lib/api.js
+++ packages/muse-express-middleware/lib/api.js
@@ -2,24 +2,71 @@
 const muse = require('muse-core');
 const _ = require('lodash');
 const logger = muse.logger.createLogger('muse-express-middleware.api');
-const apis = {
-  'am.getApp': 'get',
-  post: [],
-  put: [],
-  delete: [],
-};
-module.exports = () => {
-  return async (req, res) => {
-    //
-    const apiPath = req.path;
-    const func = _.get(muse, apiPath);
-    try {
-      if ('get') {
-      }
 
-      if ('post') {
-        await func({ ...req.body, author: 'nate' });
-      }
-    } catch (err) {}
+module.exports = ({ basePath = '/api/v2' }) => {
+  return async (req, res, next) => {
+    if (!req.path.startsWith(basePath)) {
+      return next();
+    }
+    // e.g: /api/v2/create-app
+    const apiPath = req.path.replace(basePath, '');
+
+    // api key: am.createApp
+    const apiKey = apiPath
+      .split('/')
+      .filter(Boolean)
+      .map((s) => _.camelCase(s))
+      .join('.');
+
+    if (!_.get(muse, apiKey)) {
+      return next();
+    }
+
+    // Only get apis need define args in query
+    res.setHeader('Content-Type', 'application/json');
+    if (!['get', 'post'].includes(req.method.toLowerCase())) {
+      const errMsg = `Method '${req.method}' is not allowed.`;
+      logger.error(errMsg);
+      res.write(JSON.stringify({ error: errMsg }));
+      res.statusCode = 405;
+      res.end();
+      return;
+    }
+    try {
+      await muse.utils.asyncInvoke('museExpressMiddleware.api.before', {
+        apiKey,
+        basePath,
+        req,
+        res,
+      });
+      const isGet = req.method.toLowerCase();
+      const args = isGet ? _.castArray(JSON.parse(decodeURIComponent(req.query.args))) : [req.body];
+      const result = { data: await _.invoke(muse, apiKey, ...args) };
+      await muse.utils.asyncInvoke('museExpressMiddleware.api.after', {
+        result,
+        apiKey,
+        args,
+        req,
+        res,
+        basePath,
+      });
+      res.send(JSON.stringify(result));
+      return;
+    } catch (err) {
+      const errorResult = { error: err.message, stack: err.stack || null };
+      await muse.utils.asyncInvoke('museExpressMiddleware.api.failed', {
+        err,
+        errorResult,
+        apiKey,
+        basePath,
+        req,
+        res,
+      });
+      const errMsg = JSON.stringify(errorResult);
+      logger.error(errMsg);
+      res.statusCode = err.statusCode || 500;
+      res.write(errMsg);
+      res.end();
+    }
   };
 };
--- packages/muse-simple-server/lib/server.js
+++ packages/muse-simple-server/lib/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
 const museAssetsMiddleware = require('muse-express-middleware/lib/assets');
+const museApiMiddleware = require('muse-express-middleware/lib/api');
 
 async function server({ appName, envName = 'staging', isDev, port = 6070 }) {
   const app = express();
+  app.use((req, res, next) => {
+    console.log(req.path);
+    console.log(req.query);
+    next();
+  });
+  app.use(museApiMiddleware({}));
   app.use(museAssetsMiddleware({}));
   app.get('/*', express.static(path.join(__dirname, '../static')));
   app.get('/*', require('./indexPage')(appName, envName, isDev));
