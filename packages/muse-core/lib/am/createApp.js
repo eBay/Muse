e01388d94eae22afa5141b3413bc24cd956e787f
jixguo <jixguo@ebay.com>
2022-06-30 10:48:08 +0800

    @typedef one change

--- packages/muse-core/lib/am/createApp.js
+++ packages/muse-core/lib/am/createApp.js
@@ -8,16 +8,13 @@ const logger = require('../logger').createLogger('muse.am.createApp');
  * @module muse-core/am/createApp
  */
 
-/**
- * @typedef {object} CreateAppArgument
- * @property {string} appName the app name
- * @property {string} [author=osUsername] default to the current os logged in user
- */
 
 /**
  * @description Create app in the Muse registry.
  * A app is <registry-storage>/apps/<app-name>.yaml
- * @param {CreateAppArgument} params args to create an app
+ * @param {object} params args to create an app
+ * @param {string} params.appName the app name
+ * @param {string} [params.author=osUsername] default to the current os logged in user
  */
 module.exports = async (params = {}) => {
   validate(schema, params);
