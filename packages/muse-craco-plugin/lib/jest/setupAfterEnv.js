87bcfb4fc6cdc6b5c1c65b0e53ad544a70131ad2
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2022-07-04 16:53:46 +0800

    MUSE-273 add jest libs

---
+++ packages/muse-craco-plugin/lib/jest/setupAfterEnv.js
@@ -0,0 +1,6 @@
+require('@testing-library/jest-dom/extend-expect');
+require('jest-location-mock');
+require('./__mocks__/matchMedia.mock');
+require('./__mocks__/museConfig.mock');
+require('./__mocks__/museEntries.mock');
+require('./__mocks__/prompt.mock');
--- packages/muse-craco-plugin/lib/overrideJestConfig.js
+++ packages/muse-craco-plugin/lib/overrideJestConfig.js
@@ -1,9 +1,49 @@
+const path = require('path');
+const resolveCwd = require('resolve-cwd');
+
+// Get muse libs dependencies of a plugin project  (copied from /config/ext/webpack.js)
+const getMuseLibs = (pkgJson, paths) => {
+  return Object.keys({
+    ...pkgJson.dependencies,
+    ...pkgJson.devDependencies,
+    ...pkgJson.peerDependencies,
+  }).filter(dep => {
+    try {
+      const depPkgJson = fs.readJsonSync(`${paths.appNodeModules}/${dep}/package.json`);
+      return depPkgJson.muse && depPkgJson.muse.type === 'lib';
+    } catch (e) {
+      // If failed to read Muse config, just ignore.
+      return false;
+    }
+  });
+};
+
 module.exports = ({
   jestConfig,
   cracoConfig,
   pluginOptions,
   context: { env, paths, resolve, rootDir },
 }) => {
+  const pkgJson = require(path.join(paths.appPackageJson));
+  const libModules = getMuseLibs(pkgJson, paths);
+  const esModules = ['lodash-es', 'react-syntax-highlighter'];
+  const excludedModules = [...libModules, ...esModules].join('|');
+  const moduleNameMapperForMuseLibs = {};
+  libModules.forEach(lib => {
+    moduleNameMapperForMuseLibs[`${lib}/(.*)`] = `<rootDir>/node_modules/${lib}/src/$1`;
+  });
+
+  jestConfig.moduleNameMapper = { ...jestConfig.moduleNameMapper, ...moduleNameMapperForMuseLibs };
+  jestConfig.transformIgnorePatterns.push(`/node_modules/(?!${excludedModules})`);
+  jestConfig.testURL = 'http://localhost';
+  jestConfig.testTimeout = 10000;
+  jestConfig.reporters = ['default'];
+  jestConfig.coverageReporters = ['lcov', 'text', 'cobertura'];
+  jestConfig.setupFiles.push(
+    require(resolveCwd('jest-localstorage-mock')),
+    require(resolveCwd('jest-canvas-mock')),
+  );
+  jestConfig.setupFilesAfterEnv = [require(resolveCwd('./jest/setupAfterEnv.js'))];
   console.log(JSON.stringify(jestConfig, null, 4));
 
   // Always return the config object.
