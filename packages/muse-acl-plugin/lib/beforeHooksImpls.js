ec3953e0700ecba4e07dab79d22d2fd520c49d03
Lin Gao <gling@ebay.com>
2022-07-05 16:02:51 +0800

    Implement ACL plugin based on `@casl/ability`.

---
+++ packages/muse-acl-plugin/lib/beforeHooksImpls.js
@@ -0,0 +1,79 @@
+const { ForbiddenError, subject } = require('@casl/ability');
+const {
+  deployPluginAbility: defaultDeployPluginAbility,
+  deletePluginAbility: defaultDeletePluginAbility,
+  customizedAbility,
+} = require('./ability');
+const plugin = require('js-plugin');
+
+const {
+  errorUnlessCan,
+  getAdminMembers,
+  getAppMembers,
+  getPluginMembers,
+  isMember,
+} = require('./utils');
+
+module.exports = () => {
+  plugin.register({
+    name: 'test-extend-acl',
+    museACL: {
+      deployPlugin: {
+        roles: {
+          custoizedRole: true,
+        },
+      },
+      deletePlugin: {
+        ability: customizedAbility,
+      },
+    },
+  });
+
+  return {
+    museCore: {
+      am: {
+        beforeDeleteApp: async (...args) => {},
+      },
+      pm: {
+        beforeDeployPlugin: async function() {
+          const [, params] = arguments;
+          const admins = await getAdminMembers(params);
+          const appOwners = await getAppMembers(params);
+          const pluginOwners = await getPluginMembers(params);
+          const defaultAuthorizedRoles = {
+            admin: isMember(params.author, admins),
+            appOwner: isMember(params.author, appOwners),
+            pluginOwner: isMember(params.author, pluginOwners),
+          };
+
+          const authorizedRoles =
+            plugin.invoke('!museACL.deployPlugin.roles')?.[0] || defaultAuthorizedRoles;
+
+          const deployPluginAbility =
+            plugin.invoke('!museACL.deployPlugin.ability')?.[0] || defaultDeployPluginAbility;
+
+          const isAllowed = deployPluginAbility.can('deploy', subject('plugin', authorizedRoles));
+          errorUnlessCan(isAllowed, authorizedRoles, 'plugin', 'deploy', params);
+        },
+
+        deletePlugin: async function(...args) {
+          const [, params] = arguments;
+          const admins = await getAdminMembers(params);
+          const pluginOwners = await getPluginMembers(params);
+          const defaultAuthorizedRoles = {
+            admin: isMember(params.author, admins),
+            pluginOwner: isMember(params.author, pluginOwners),
+          };
+          const authorizedRoles =
+            plugin.invoke('!museACL.deletePlugin.roles')?.[0] || defaultAuthorizedRoles;
+
+          const deletePluginAbility =
+            plugin.invoke('!museACL.deletePlugin.ability')?.[0] || defaultDeletePluginAbility;
+
+          const isAllowed = deletePluginAbility.can('delete', subject('plugin', authorizedRoles));
+          errorUnlessCan(isAllowed, authorizedRoles, 'delete', 'deploy', params);
+        },
+      },
+    },
+  };
+};
