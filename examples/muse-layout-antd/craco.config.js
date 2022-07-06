cf8b5a79a2f16e130fbdc27cf7df18883245b62d
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2022-07-06 13:44:32 +0800

    MUSE-273 example with muse-layout

--- examples/muse-layout-antd/craco.config.js
+++ examples/muse-layout-antd/craco.config.js
@@ -3,6 +3,17 @@ const MuseCracoPlugin = require('@ebay/muse-craco-plugin');
 
 module.exports = () => {
   return {
-    plugins: [{ plugin: CracoLessPlugin }, { plugin: MuseCracoPlugin }],
+    plugins: [
+      { plugin: CracoLessPlugin },
+      { plugin: MuseCracoPlugin, options: { showJestConfig: true } },
+    ],
+    jest: {
+      configure: {
+        // override default jest configuration provided by @ebay/muse-craco-plugin
+        setupFiles: ['<rootDir>/tests/setup.js'],
+        testMatch: ['<rootDir>/tests/**/*.test.js'],
+        roots: ['<rootDir>/tests/'],
+      },
+    },
   };
 };
