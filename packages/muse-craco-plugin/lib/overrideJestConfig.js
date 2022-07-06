97aefe9683d77c300f0ca6ee98b0db0d36d04b4c
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2022-07-06 11:43:10 +0800

    MUSE-273 clean code

--- packages/muse-craco-plugin/lib/overrideJestConfig.js
+++ packages/muse-craco-plugin/lib/overrideJestConfig.js
@@ -10,7 +10,7 @@ const getMuseLibs = (pkgJson, paths) => {
   }).filter(dep => {
     try {
       const depPkgJson = fs.readJsonSync(`${paths.appNodeModules}/${dep}/package.json`);
-      return depPkgJson.muse && depPkgJson.muse.type === 'lib';
+      return depPkgJson?.muse?.type === 'lib';
     } catch (e) {
       // If failed to read Muse config, just ignore.
       return false;
@@ -33,24 +33,24 @@ module.exports = ({
   });
 
   // #1 assign module mappers for muse libs, so that references to lib modules/files on the plugin being tested can be found by Jest
-  // #2 force lib plugins source to be transpiled by babel (by default all modules under /node_modules is ignored)
   jestConfig.moduleNameMapper = { ...jestConfig.moduleNameMapper, ...moduleNameMapperForMuseLibs };
+
+  // #2 force lib plugins source to be transpiled by babel (by default all modules under /node_modules is ignored)
   if (excludedModules.length > 0) {
     jestConfig.transformIgnorePatterns.push(`/node_modules/(?!(${excludedModules})/)`);
   }
 
   // #3 add default reporter if none has been configured on craco.config.js from the plugin under test
-  jestConfig.reporters =
-    jestConfig.reporters && jestConfig.reporters.length > 0 ? jestConfig.reporters : ['default'];
+  jestConfig.reporters = jestConfig?.reporters?.length > 0 ? jestConfig.reporters : ['default'];
 
   // #4 add default mocks for MUSE_GLOBAL / MUSE_ENTRIES if no "setupFilesAfterEnv" has been configured on craco.config.js from the plugin under test
   jestConfig.setupFilesAfterEnv =
-    jestConfig.setupFilesAfterEnv && jestConfig.setupFilesAfterEnv.length > 0
+    jestConfig?.setupFilesAfterEnv?.length > 0
       ? jestConfig.setupFilesAfterEnv
       : [require.resolve('./jest/setupAfterEnv.js')];
 
   // #5 optionally show Jest configuration for debugging purposes, if the plugin option "showJestConfig" gets passed along
-  if (pluginOptions && pluginOptions.showJestConfig) {
+  if (pluginOptions?.showJestConfig) {
     console.log(JSON.stringify(jestConfig, null, 4));
   }
 
