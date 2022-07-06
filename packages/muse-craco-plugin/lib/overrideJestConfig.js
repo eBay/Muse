4d0dfe9a6570894428e7fa7688ca10d72e113635
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2022-07-06 10:51:12 +0800

    MUSE-273 add documentation on source code

--- packages/muse-craco-plugin/lib/overrideJestConfig.js
+++ packages/muse-craco-plugin/lib/overrideJestConfig.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-// Get muse libs dependencies of a plugin project  (copied from /config/ext/webpack.js)
+// Get muse libs dependencies of a plugin project
 const getMuseLibs = (pkgJson, paths) => {
   return Object.keys({
     ...pkgJson.dependencies,
@@ -32,18 +32,24 @@ module.exports = ({
     moduleNameMapperForMuseLibs[`${lib}/(.*)`] = `<rootDir>/node_modules/${lib}/src/$1`;
   });
 
+  // #1 assign module mappers for muse libs, so that references to lib modules/files on the plugin being tested can be found by Jest
+  // #2 force lib plugins source to be transpiled by babel (by default all modules under /node_modules is ignored)
   jestConfig.moduleNameMapper = { ...jestConfig.moduleNameMapper, ...moduleNameMapperForMuseLibs };
   if (excludedModules.length > 0) {
     jestConfig.transformIgnorePatterns.push(`/node_modules/(?!(${excludedModules})/)`);
   }
+
+  // #3 add default reporter if none has been configured on craco.config.js from the plugin under test
   jestConfig.reporters =
     jestConfig.reporters && jestConfig.reporters.length > 0 ? jestConfig.reporters : ['default'];
 
+  // #4 add default mocks for MUSE_GLOBAL / MUSE_ENTRIES if no "setupFilesAfterEnv" has been configured on craco.config.js from the plugin under test
   jestConfig.setupFilesAfterEnv =
     jestConfig.setupFilesAfterEnv && jestConfig.setupFilesAfterEnv.length > 0
       ? jestConfig.setupFilesAfterEnv
       : [require.resolve('./jest/setupAfterEnv.js')];
 
+  // #5 optionally show Jest configuration for debugging purposes, if the plugin option "showJestConfig" gets passed along
   if (pluginOptions && pluginOptions.showJestConfig) {
     console.log(JSON.stringify(jestConfig, null, 4));
   }
