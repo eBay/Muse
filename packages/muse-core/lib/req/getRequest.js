ac43c2e00e26b458965cf4ba2f25f68cf74e7e97
jixguo <jixguo@ebay.com>
2022-07-15 11:53:03 +0800

    update schema.json

--- packages/muse-core/lib/req/getRequest.js
+++ packages/muse-core/lib/req/getRequest.js
@@ -1,6 +1,5 @@
-const { asyncInvoke, jsonByYamlBuff, validate } = require('../utils');
+const { asyncInvoke, jsonByYamlBuff } = require('../utils');
 const { registry } = require('../storage');
-const schema = require('../schemas/req/getRequest.json');
 
 /**
  * @module muse-core/req/getRequest
@@ -12,19 +11,20 @@ const schema = require('../schemas/req/getRequest.json');
  * @returns {Buffer} Buffer of request.
  */
 module.exports = async requestId => {
-  validate(schema, requestId);
-  const ctx = {};
-  await asyncInvoke('museCore.req.beforeGetRequest', ctx, requestId);
+  if (requestId) {
+    const ctx = {};
+    await asyncInvoke('museCore.req.beforeGetRequest', ctx, requestId);
 
-  try {
-    const keyPath = `/requests/${requestId}.yaml`;
-    ctx.request = jsonByYamlBuff(await registry.get(keyPath));
-    await asyncInvoke('museCore.req.getRequest', ctx, requestId);
-  } catch (err) {
-    ctx.error = err;
-    await asyncInvoke('museCore.req.failedGetRequest', ctx, requestId);
-    throw err;
+    try {
+      const keyPath = `/requests/${requestId}.yaml`;
+      ctx.request = jsonByYamlBuff(await registry.get(keyPath));
+      await asyncInvoke('museCore.req.getRequest', ctx, requestId);
+    } catch (err) {
+      ctx.error = err;
+      await asyncInvoke('museCore.req.failedGetRequest', ctx, requestId);
+      throw err;
+    }
+    await asyncInvoke('museCore.req.afterGetRequest', ctx, requestId);
+    return ctx.request;
   }
-  await asyncInvoke('museCore.req.afterGetRequest', ctx, requestId);
-  return ctx.request;
 };
