cb9629eef21540862382ef564bdc941ab48f8211
Nate Wang <pwang7@ebay.com>
2022-07-15 15:34:58 +0800

    Refine muse config mechanism.

--- workspace/packages/muse-core/lib/config.js
+++ workspace/packages/muse-core/lib/config.js
@@ -1,7 +1,6 @@
 const fs = require('fs-extra');
 const _ = require('lodash');
 const { cosmiconfigSync } = require('cosmiconfig');
-const os = require('os');
 
 const explorerSync = cosmiconfigSync('muse', {
   searchPlaces: [
@@ -13,43 +12,41 @@ const explorerSync = cosmiconfigSync('muse', {
     'muse.config.cjs',
     'muse.config.json',
   ],
+  // Only load config from the cwd by default
+  stopDir: process.cwd(),
 });
 
 const envConfigFile = process.env.MUSE_CONFIG_FILE;
 
 let cosmicResult;
 if (envConfigFile) {
-  if (!fs.existsSync(envConfigFile)) {
-    // If the config file is by env.MUSE_CONFIG_FILE but it doesn't exist, throw the error.
-    throw new Error(
-      `Muse config file specified by MUSE_CONFIG_FILE doesn't exist: ${envConfigFile}.`,
-    );
-  }
-  cosmicResult = explorerSync.load(envConfigFile);
+  envConfigFile
+    .split(';')
+    .filter(Boolean)
+    .map(_.trim)
+    .some(f => {
+      if (fs.existsSync(envConfigFile)) {
+        cosmicResult = explorerSync.load(f);
+        return true;
+      }
+    });
 } else {
   cosmicResult = explorerSync.search();
-  if (!cosmicResult) {
-    cosmicResult = explorerSync.search(os.homedir());
-  }
-}
-
-if (cosmicResult) {
-  // console.log(`Loaded Muse config from: ${cosmicResult.filepath}.`);
 }
 
 let config = cosmicResult?.config || {};
 if (_.isFunction(config)) {
   config = config();
 }
-// process.exit();
+
 if (config.extend) {
   let baseConfig = require(config.extend);
   if (_.isFunction(baseConfig)) baseConfig = baseConfig();
   Object.assign(baseConfig, _.omit(config, ['extend', 'plugins', 'presets']));
   if (!baseConfig.plugins) baseConfig.plugins = [];
   if (!baseConfig.presets) baseConfig.presets = [];
-  baseConfig.plugins.concat(..._(config.plugins || []));
-  baseConfig.presets.concat(..._(config.presets || []));
+  baseConfig.plugins.push(...(config.plugins || []));
+  baseConfig.presets.push(...(config.presets || []));
   config = baseConfig;
 }
 
