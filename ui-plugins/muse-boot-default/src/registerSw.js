efb9cd7c6288901776af4517cd13737c64b5efc0
Nate Wang <pwang7@ebay.com>
2022-07-17 13:45:02 +0800

    Refine service worker.

--- ui-plugins/muse-boot-default/src/registerSw.js
+++ ui-plugins/muse-boot-default/src/registerSw.js
@@ -1,22 +1,9 @@
 // Try to register service worker.
 import loading from './loading';
 export default function() {
-  const { app } = window.MUSE_GLOBAL;
+  const { serviceWorker } = window.MUSE_GLOBAL;
   if (!navigator.serviceWorker) return;
-  if (app?.noServiceWorker) {
-    // TODO: should we unregister sw? It just means it doesn't use Muse's sw.
-    // navigator.serviceWorker
-    //   .getRegistrations()
-    //   .then(registrations => {
-    //     for (let registration of registrations) {
-    //       registration.unregister();
-    //     }
-    //   })
-    //   .catch(function(err) {
-    //     console.log('Service Worker un-registration failed: ', err);
-    //     return Promise.resolve();
-    //   });
-  } else if (document.location.protocol === 'https:') {
+  if (serviceWorker && document.location.protocol === 'https:') {
     loading.showMessage('Registering Muse service worker.');
     return new Promise(resolve => {
       let resolved = false;
@@ -27,7 +14,7 @@ export default function() {
         }
       }, 10000);
       navigator.serviceWorker
-        .register(app.serviceWorker || '/sw.js', {})
+        .register(serviceWorker, {})
         .then(function() {
           resolved = true;
           console.log('Service Worker register done');
