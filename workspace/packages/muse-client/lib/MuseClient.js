8c19829118025e3ecc484f7d2d450fe11b7bc65a
Nate Wang <pwang7@ebay.com>
2022-07-19 18:57:44 +0800

    New muse client

--- workspace/packages/muse-client/lib/MuseClient.js
+++ workspace/packages/muse-client/lib/MuseClient.js
@@ -1,50 +1,45 @@
 const axios = require('axios');
 const kebabCase = require('lodash/fp/kebabCase');
 
+// A muse-core API wrapper to use an existing Muse API service.
+// It uses post method for all requests.
 class MuseClient {
   constructor({ endpoint, token, axiosConfig }) {
-    this.client = axios.create({
+    const client = axios.create({
       baseURL: endpoint,
       timeout: 30000,
       ...axiosConfig,
+      headers: {
+        authorization: token || '',
+      },
+      ...axiosConfig.headers,
     });
-    this.token = token;
-  }
+    const post = async (apiPath, args) => {
+      try {
+        const res = await client.post(apiPath, { args });
+        return res.data?.data;
+      } catch (err) {
+        //
+        if (err?.response?.data?.error) {
+          const err2 = new Error(err?.response?.data?.error);
+          throw err2;
+        } else throw err;
+      }
+    };
 
-  async get(name, args, config) {
-    if (!Array.isArray(args)) args = [args];
-    const res = await this.client.get(
-      name
-        .split('.')
-        .map(kebabCase)
-        .join('/'),
-      {
-        params: { args: encodeURIComponent(JSON.stringify(args || [])) },
-        ...config,
-        headers: {
-          authorization: this.token || '',
-          ...config?.headers,
-        },
+    // Construct the API path by a javascript proxy
+    const handler = {
+      get(target, prop, receiver) {
+        if (prop === '_api_path_') return target._api_path_ || '';
+        const apiPath = (receiver._api_path_ || '') + '/' + kebabCase(prop);
+        const func = async (...args) => {
+          return await post(apiPath, args);
+        };
+        func._api_path_ = apiPath;
+        return new Proxy(func, handler);
       },
-    );
-    return res.data?.data;
-  }
-  async post(name, args, config) {
-    const res = await this.client.post(
-      name
-        .split('.')
-        .map(kebabCase)
-        .join('/'),
-      args || {},
-      {
-        ...config,
-        headers: {
-          authorization: this.token || '',
-          ...config?.headers,
-        },
-      },
-    );
-    return res.data?.data;
+    };
+    return new Proxy(this, handler);
   }
 }
 
--- workspace/packages/muse-client/test.js
+++ workspace/packages/muse-client/test.js
@@ -2,7 +2,7 @@ const MuseClient = require('./lib/MuseClient');
 const httpsAgent = new (require('https').Agent)({
   rejectUnauthorized: false,
 });
-const client = new MuseClient({
+const muse = new MuseClient({
   endpoint: 'http://localhost:6070/api/v2', // 'https://musenextsvc.vip.qa.ebay.com/api/v2',
   axiosConfig: {
     httpsAgent,
@@ -10,7 +10,15 @@ const client = new MuseClient({
 });
 
 (async () => {
-  // const data = await client.get('am.getApp', 'nateapp');
-  const data = await client.post('am.createApp', { appName: 'nateapp5' });
-  console.log(data);
+  try {
+    // const data = await muse.a.b.c.d.tes1t();
+    // const data = await muse.data.get('muse.app.nateapp');
+    // const data = await muse.am.getApps('muse.npm.versions');
+    const data = await muse.am.createApp({ appName: 'app9' });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+    console.log(err?.response?.data?.error);
+    console.log(err.message);
+  }
 })();
