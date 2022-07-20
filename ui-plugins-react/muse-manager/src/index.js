7d293f22ee32aa2e5be0ce30148aad811d9b79f9
Nate Wang <pwang7@ebay.com>
2022-07-20 16:01:44 +0800

    Adding new muse manager.

---
+++ ui-plugins-react/muse-manager/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+let rootNode = document.getElementById('root');
+if (!rootNode) {
+  rootNode = document.createElement('div');
+  rootNode.id = 'root';
+  document.body.appendChild(rootNode);
+}
+
+const root = ReactDOM.createRoot(rootNode);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
