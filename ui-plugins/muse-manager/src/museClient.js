851688da53e69dfd57394accc152c5d1cda0100a
Nate Wang <pwang7@ebay.com>
2022-08-07 20:00:51 +0800

    Enlarge timeout

--- ui-plugins/muse-manager/src/museClient.js
+++ ui-plugins/muse-manager/src/museClient.js
@@ -5,5 +5,8 @@ const museClient = mClient.create({
   endpoint: 'http://localhost:6070/api/v2',
   // endpoint: 'http://localhost:8080/api/v2',
   token: window.MUSE_GLOBAL.getUser().museSession,
+  axiosConfig: {
+    timeout: 120000,
+  },
 });
 export default museClient;
