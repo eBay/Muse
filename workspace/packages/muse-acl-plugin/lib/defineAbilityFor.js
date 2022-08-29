9064719b54f47d93471d6880c3251e51b22e6148
Nate Wang <pwang7@ebay.com>
2022-08-29 14:51:24 +0800

    Acl refinement.

--- workspace/packages/muse-acl-plugin/lib/defineAbilityFor.js
+++ workspace/packages/muse-acl-plugin/lib/defineAbilityFor.js
@@ -1,9 +1,38 @@
-import { defineAbility } from '@casl/ability';
+import { AbilityBuilder, Ability } from '@casl/ability';
+import jsPlugin from 'js-plugin';
 
-export default function defineAbilityFor({ user, app, plugin }) {
-  return defineAbility((allow, forbid) => {
-    allow('build', 'Plugin', { published: true });
-    allow('deploy', 'Plugin', { published: true });
-    allow('read', 'Article', { published: false, sharedWith: user.id });
+export default function defineAbilityFor({ user }) {
+  const { can: allow, cannot: forbid, build } = new AbilityBuilder(Ability);
+
+  jsPlugin.invoke('museAcl.beforeDefineAbility', { user, allow, forbid });
+
+  // Muse admin could manage all
+  if (user.isMuseAdmin) {
+    allow('manage', 'all');
+  }
+
+  // Everyone could create app or plugin
+  allow('create', 'Plugin');
+  allow('create', 'App');
+
+  // Plugin owners can update, build plugin
+  // manage means: 'update', 'build', 'config', 'delete'...
+  allow('manage', 'Plugin', { owners: user.username });
+
+  // App owners could manage app, envs
+  // App owners could also deploy/undeploy plugins for the app
+  // manage means: 'update', 'delete', 'create-env', 'update-env', 'delete-env', 'deploy', 'undeploy'...
+  allow('manage', 'App', { owners: user.username });
+
+  // For deployed plugin, app owners or plugin owners could manage them
+  // manage means: 'edit-config', 'set-variables', ...
+  allow('manage', 'DeployedPlugin', {
+    'app.owners': user.username,
+    'plugin.owners': user.username,
   });
+
+  jsPlugin.invoke('museAcl.defineAbility', { user, allow, forbid });
+  jsPlugin.invoke('museAcl.afterDefineAbility', { user, allow, forbid });
+
+  return build();
 }
