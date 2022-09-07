b99be580f2aaba04a1b04ff06b3b2e250c211d86
Nate Wang <pwang7@ebay.com>
2022-09-07 20:09:08 +0800

    check auth expiry

--- ui-plugins/muse-manager/src/museClient.js
+++ ui-plugins/muse-manager/src/museClient.js
@@ -4,7 +4,7 @@ const museClient = mClient.create({
   endpoint: 'https://musenextsvc.vip.qa.ebay.com/api/v2',
   // endpoint: 'http://localhost:6070/api/v2',
   // endpoint: 'http://localhost:8080/api/v2',
-  token: window.MUSE_GLOBAL.getUser().museSession + 1,
+  token: window.MUSE_GLOBAL.getUser().museSession,
   axiosConfig: {
     timeout: 120000,
   },
