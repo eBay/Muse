da6f58573402ef31668dc63076865219f6cf74a8
Nate Wang <pwang7@ebay.com>
2022-10-05 15:38:09 +0800

    Refine muse manager endpoint

--- ui-plugins/muse-manager/src/museClient.js
+++ ui-plugins/muse-manager/src/museClient.js
@@ -3,9 +3,9 @@ import mClient from '@ebay/muse-client';
 const endpoint =
   window.MUSE_GLOBAL.getPluginVariables('@ebay/muse-manager')?.museApiEndpoint ||
   window.MUSE_GLOBAL.getAppVariables()?.museApiEndpoint ||
-  window.MUSE_GLOBAL.isLocal
+  (window.MUSE_GLOBAL.isLocal
     ? 'http://localhost:6070/api/v2'
-    : 'https://musenextsvc.vip.qa.ebay.com/api/v2';
+    : 'https://musenextsvc.vip.qa.ebay.com/api/v2');
 const museClient = mClient.create({
   // endpoint: 'https://musenextsvc.vip.qa.ebay.com/api/v2',
   // endpoint: 'http://localhost:6070/api/v2',
