97d900168a1d6e99424d40ec0ecb68ef691350ad
Nate Wang <pwang7@ebay.com>
2022-10-10 16:29:17 +0800

    update eyes

--- examples/demo-controller-plugin/src/components/MovingEyes.js
+++ examples/demo-controller-plugin/src/components/MovingEyes.js
@@ -1,16 +1,12 @@
-import { useRef, useEffect, useCallback } from 'react';
-import { Button, Popover, Switch } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { useRef, useCallback } from 'react';
 import { useEvent } from 'react-use';
 import _ from 'lodash';
 import './MovingEyes.less';
 
 export const MovingEyes = () => {
-  const ref1 = useRef();
-  const ref2 = useRef();
+  const ref = useRef();
   const handleMouseMove = useCallback(evt => {
-    [ref1, ref2].forEach(r => {
-      const node = r.current;
+    _.forEach(ref.current.children, node => {
       const rect = node.getBoundingClientRect();
       const x = rect.x + rect.width / 2;
       const y = rect.y + rect.height / 2;
@@ -21,9 +17,8 @@ export const MovingEyes = () => {
   }, []);
   useEvent('mousemove', handleMouseMove);
   return (
-    <div className="moving-eyes">
-      <div className="eye-item" ref={ref1}></div>
-      <div className="eye-item" ref={ref2}></div>
+    <div className="moving-eyes" ref={ref}>
+      <div className="eye-item"></div>
     </div>
   );
 };
