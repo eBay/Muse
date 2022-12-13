39242ac251188be4837b45ccf52a3a2b53eada5d
Lin Gao <gling@ebay.com>
2022-12-13 13:51:10 +0800

    Record build time in info.json when building.

--- workspace/packages/muse-scripts-react/bin/muse-scripts-react.js
+++ workspace/packages/muse-scripts-react/bin/muse-scripts-react.js
@@ -5,7 +5,7 @@ const spawn = require('cross-spawn');
 const resolveCwd = require('resolve-cwd');
 
 const args = process.argv.slice(2);
-const scriptIndex = args.findIndex(x => x === 'build' || x === 'start' || x === 'test');
+const scriptIndex = args.findIndex((x) => x === 'build' || x === 'start' || x === 'test');
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 
 if (script === 'build' && args.includes('--dev')) {
---
+++ workspace/packages/muse-webpack-plugin/lib/MuseBuildPlugin.js
@@ -0,0 +1,31 @@
+'use strict';
+const fs = require('fs-extra');
+const path = require('path');
+
+/**
+ * This plugin will generate a info.json under output folder, recording the buildTime property against the building env.
+ */
+class MuseBuildPlugin {
+  constructor(context, entries, options) {
+    console.log('context: ', context);
+    this.context = context;
+    this.entries = entries;
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.done.tap('MuseBuildPlugin', (stats) => {
+      const buildPath = process.env.BUILD_PATH;
+      const buildTime = stats.endTime - stats.startTime;
+      const infoJsonFilePath = path.join(buildPath, 'info.json');
+      fs.removeSync(infoJsonFilePath);
+      fs.ensureFileSync(infoJsonFilePath);
+      const info = {
+        buildTime,
+      };
+      fs.writeFileSync(infoJsonFilePath, JSON.stringify(info, null, 2));
+    });
+  }
+}
+
+module.exports = MuseBuildPlugin;
