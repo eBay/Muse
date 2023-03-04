960b3a2cb89e89bb540016cc5fcb6c3e70859253
Nate Wang <pwang7@ebay.com>
2023-03-04 22:17:37 +0800

    Code clean

--- ui-plugins/muse-lib-react/src/features/sub-app/urlUtils.js
+++ ui-plugins/muse-lib-react/src/features/sub-app/urlUtils.js
@@ -15,17 +15,6 @@ export default {
       return s;
     }
     return null;
-    // if (url.startsWith(path)) {
-    //   let u = url.replace(path, '');
-    //   if (!u.startsWith('/')) u = '/' + u;
-    //   // https://abc.com/ab/def
-    //   const arr = subAppUrl.split('/');
-    //   arr.splice(0, 3);
-    //   let s = arr.join('/').replace(/\/*$/, '') + u;
-    //   if (!s.startsWith('/')) s = '/' + s;
-    //   return s;
-    // }
-    // return null;
   },
   getBaseUrl(url) {
     const arr = url.split('/');
