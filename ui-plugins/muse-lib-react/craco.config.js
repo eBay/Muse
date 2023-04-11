f493d88999cecf4845c3fecfdb9faef08a1a268a
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-04-11 20:09:10 +0800

    update craco config

--- ui-plugins/muse-lib-react/craco.config.js
+++ ui-plugins/muse-lib-react/craco.config.js
@@ -1,14 +1,13 @@
 const CracoLessPlugin = require('craco-less');
 const MuseCracoPlugin = require('@ebay/muse-craco-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MuseEbayCracoPlugin = require('@ebay/muse-ebay-craco-plugin');
 
 module.exports = () => {
   return {
-    plugins: [{ plugin: CracoLessPlugin }, { plugin: MuseCracoPlugin }],
-    webpack: {
-      plugins: {
-        add: [new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false })],
-      },
-    },
+    plugins: [
+      { plugin: CracoLessPlugin },
+      { plugin: MuseCracoPlugin },
+      { plugin: MuseEbayCracoPlugin },
+    ],
   };
 };
