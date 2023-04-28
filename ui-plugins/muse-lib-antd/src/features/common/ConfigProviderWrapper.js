632341b03cdfa3f00f821dbd3a8d1e7def8f021f
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-04-28 17:24:01 +0800

    simplify config provider

--- ui-plugins/muse-lib-antd/src/features/common/ConfigProviderWrapper.js
+++ ui-plugins/muse-lib-antd/src/features/common/ConfigProviderWrapper.js
@@ -1,19 +1,9 @@
-import { ConfigProvider, theme } from 'antd';
+import { ConfigProvider } from 'antd';
 
 export default function ConfigProviderWrapper({ children }) {
-  const { defaultAlgorithm, darkAlgorithm } = theme;
-  const { app } = window.MUSE_GLOBAL;
-  const isDarkMode =
-    app?.config?.theme === 'dark' || localStorage.getItem('muse-lib-antd.theme.dark')
-      ? localStorage.getItem('muse-lib-antd.theme.dark') === 'false'
-        ? false
-        : true
-      : false;
-
   return (
     <ConfigProvider
       theme={{
-        algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
         token: {
           borderRadius: 0,
         },
