7cb4f237fc3786cbe0395f1fbcd9c40423e81503
Nate Wang <pwang7@ebay.com>
2023-08-14 15:55:19 +0800

    Adding deps check.

---
+++ workspace/packages/muse-dev-utils/lib/checkLibDeps.js
@@ -0,0 +1,7 @@
+// A normal plugin project should not specify a different version
+// of some dep which is already specified in some lib plugin.
+// This function checks if there is any such case.
+
+export function checkLibDeps() {
+  // get all lib plugins
+}
