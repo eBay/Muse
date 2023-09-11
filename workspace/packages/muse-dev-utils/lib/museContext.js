1819190eb8c232d797db14b29ee63e4bfc03cefe
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-09-11 11:55:38 +0800

    MUSE-528 fix isDevBuild without MUSE_DEV_BUILD

--- workspace/packages/muse-dev-utils/lib/museContext.js
+++ workspace/packages/muse-dev-utils/lib/museContext.js
@@ -3,8 +3,8 @@ const pkgJson = require(path.join(process.cwd(), './package.json'));
 
 module.exports = {
   isTestBuild: !!process.env.MUSE_TEST_BUILD,
-  isDevBuild: !!process.env.MUSE_DEV_BUILD,
-  isDev: process.env.NODE_ENV === 'development' && !process.env.MUSE_DEV_BUILD,
+  isDevBuild: process.env.npm_lifecycle_event === 'build:dev',
+  isDev: process.env.NODE_ENV === 'development' && process.env.npm_lifecycle_event !== 'build:dev',
   isProdBuild: process.env.NODE_ENV === 'production',
   pkgJson,
   museConfig: pkgJson.muse || {},
