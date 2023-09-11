7dbcf17796ff7dce3cff75aa215db51a0bf265ab
Nate Wang <pwang7@ebay.com>
2023-09-11 08:17:02 +0800

    add eslint config

---
+++ workspace/packages/muse-modules-analyzer/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ['jest'],
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
