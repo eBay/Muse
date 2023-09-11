e2b69fa1e54578b85daef7c0e45a4a8c61d4a26b
Nate Wang <pwang7@ebay.com>
2023-09-11 21:20:35 +0800

    Validate fixed versions

---
+++ workspace/packages/muse-dev-utils/scripts/checkFixedVersions.js
@@ -0,0 +1,44 @@
+const fs = require('fs-extra');
+const path = require('path');
+const chalk = require('chalk');
+const utils = require('../lib/utils');
+const { parseMuseId } = require('@ebay/muse-modules');
+
+/**
+ * Should only run this script after build.
+ */
+
+console.log('Checking deps versions...');
+const libs = {};
+const libManifest = fs.readJsonSync(path.join(process.cwd(), `./build/dist/lib-manifest.json`));
+Object.keys(libManifest.content).forEach((mid) => {
+  const m = parseMuseId(mid);
+  libs[m.name] = m.version.join('.');
+});
+// console.log(libs);
+const pkgJson = utils.getPkgJson();
+
+const invalidDeps = {};
+[pkgJson.dependencies, pkgJson.devDependencies, pkgJson.peerDependencies].forEach((deps) => {
+  Object.keys(libs).forEach((name) => {
+    if (deps && deps[name] && deps[name] !== libs[name]) {
+      invalidDeps[name] = deps[name];
+    }
+  });
+});
+
+const error = (msg) => console.log(chalk.red(msg));
+
+if (Object.keys(invalidDeps).length) {
+  error('Invalid Muse lib dependencies:');
+  error(' 1. Shared dependencies should use fixed versions.');
+  error(' 2. The versions in build result should be the same as the ones in lib plugins.');
+  console.log('');
+  error('Please fix:');
+  Object.entries(invalidDeps).forEach(([name, version]) => {
+    error(` - ${name}: ${version} -> ${libs[name]}`);
+  });
+  error('✖ Deps versions check failed.');
+} else {
+  console.log('✔ Deps versions check passed.');
+}
---
+++ workspace/packages/muse-modules-analyzer/scripts/checkLibDeps.js
@@ -0,0 +1,109 @@
+const fs = require('fs-extra');
+const _ = require('lodash');
+const path = require('path');
+const resolveCwd = require('resolve-cwd');
+// const utils = require('./utils');
+const devUtils = require('@ebay/muse-dev-utils');
+
+// function parseMuseId(museId) {
+//   try {
+//     const m = /((^@[^/]+\/)?([^@/]+))@(\d+)\.(\d+)\.(\d+)([^./][^/]*)?\/(.+)$/.exec(museId);
+//     if (!m) return null;
+//     return {
+//       name: m[1],
+//       path: m[8],
+//       id: `${m[1]}/${m[8]}`,
+//       museId,
+//       version: m.slice(4, 7).map(Number),
+//     };
+//   } catch (err) {
+//     return null;
+//   }
+// }
+
+/**
+ *
+ * A plugin project should not specify a different version
+ * of some dep which is already specified in some lib plugin.
+ * This function checks if there is any such case.
+ * @param {*} type
+ */
+function checkLibDeps() {
+  // get all lib plugins
+  const libs = {};
+  devUtils
+    .getMuseLibs()
+    .map((lib) => lib.name)
+    .forEach((lib) => {
+      const libManifest = fs.readJsonSync(resolveCwd(`${lib}/build/${type}/lib-manifest.json`));
+      Object.keys(libManifest.content).forEach((mid) => {
+        const m = parseMuseId(mid);
+        const vObj = { from: lib, version: m.version.join('.') };
+        if (libs[m.name]) libs[m.name].push(vObj);
+        else libs[m.name] = [vObj];
+        libs[m.name] = _.uniqBy(libs[m.name], (o) => o.version);
+      });
+    });
+
+  // console.log(libs);
+
+  const pkgJson = utils.getPkgJson();
+  const customLibs = pkgJson.muse?.customLibs || [];
+  [pkgJson.dependencies, pkgJson.devDependencies, pkgJson.peerDependencies].forEach((deps) => {
+    Object.entries(deps || {}).forEach(([name, version]) => {
+      if (customLibs.includes(name)) return;
+      if (libs[name] && !libs[name].find((o) => o.version === version.replace(/^[^0-9]*/, ''))) {
+        throw new Error(
+          `Invalid Muse dependencies: all specified dependencies versions in package.json \
+should not be different with which in lib plugins. \
+Found ${name}@${version} in package.json but it should be ${libs[name]
+            ?.map((o) => `${o.version} from ${o.from}`)
+            .join(' or ')}./
+          `,
+        );
+      }
+    });
+  });
+  console.log('✅ Lib deps check passed.');
+}
+
+/**
+ * All deps in lib plugins should use fixed versions.
+ * This check should be used after build so that the build folder exists.
+ *
+ * @param {*} folder - The plugin folder.
+ */
+function getUnfixedVersions(folder = process.cwd()) {
+  const libs = {};
+  const libManifest = fs.readJsonSync(path.join(folder, `./build/dist/lib-manifest.json`));
+  Object.keys(libManifest.content).forEach((mid) => {
+    const m = parseMuseId(mid);
+    libs[m.name] = true;
+  });
+  // console.log(libs);
+  const pkgJson = utils.getPkgJson();
+
+  const invalidDeps = {};
+  [pkgJson.dependencies, pkgJson.devDependencies, pkgJson.peerDependencies].forEach((deps) => {
+    Object.keys(libs).forEach((name) => {
+      if (deps && deps[name] && !/^\d/.test(deps[name])) {
+        invalidDeps[name] = deps[name];
+      }
+    });
+  });
+
+  if (Object.keys(invalidDeps).length) {
+    throw new Error(
+      `Invalid Muse lib dependencies: all lib dependencies should be fixed versions. Please fix:\n\r ${JSON.stringify(
+        invalidDeps,
+        null,
+        2,
+      )}`,
+    );
+  }
+  console.log('✅ Deps versions check passed.');
+}
+
+// checkLibDeps();
+// ensureAllLibDepsUseFixedVersions();
+// module.exports = { checkLibDeps, ensureAllLibDepsUseFixedVersions };
