c250037e4accb622855ef846fdf35ab252c79011
Nate Wang <pwang7@ebay.com>
2023-09-16 11:18:35 +0800

    Refine find module logic

--- workspace/packages/muse-modules/findMuseModule.js
+++ workspace/packages/muse-modules/findMuseModule.js
@@ -4,18 +4,26 @@ const buildCache = require('./buildCache');
 // Get the version diff, e.g: [1,2,3] - [1,2,4] = [0,0,1]
 const verDiff = (v1, v2) => [0, 1, 2].map((i) => Math.abs(v1[i] - v2[i]));
 
-// Whether diff1 is less than diff2, e.g: [1,2,3] < [1,2,4]
-const diffLt = (d1, d2) => {
+// Whether ver1 is less than ver2, e.g: [1,2,3] < [1,2,4]
+// This can also be used to compare version diff from verDiff()
+const lt = (v1, v2) => {
   for (let i = 0; i < 3; i++) {
-    if (d1[i] < d2[i]) return true;
-    if (d1[i] > d2[i]) return false;
+    if (v1[i] < v2[i]) return true;
+    if (v1[i] > v2[i]) return false;
   }
   return false;
 };
 
+const gt = (v1, v2) => !lt(v1, v2);
+
 /**
- * Resolve module to the closest semantic version
- * @param {String} museId example: @ebay/nice-modal@1.0.0/src/index.js
+ * Resolve module to the greater and closest semantic version.
+ * If there is only one version, use it.
+ * If there are multiple versions, use the closest but greater one.
+ *
+ * NOTE: it doesn't support prerelease versions. e.g: 1.0.0-beta.1 will be treated as 1.0.0
+ *
+ * @param {String} museId example: @ebay/nice-modal-react@1.0.0/src/index.js
  * @param {*} museSharedModules { modules, cache }
  * @returns
  */
@@ -26,6 +34,7 @@ function findMuseModule(museId, museShared) {
   let cache = museShared.cache;
 
   if (!cache) {
+    // group different versions by module id
     buildCache(museShared);
     cache = museShared.cache;
   }
@@ -36,19 +45,31 @@ function findMuseModule(museId, museShared) {
   const candidates = cache[m.id];
   if (!candidates) return null;
   if (candidates.length === 1) return museShared.modules[candidates[0].museId];
-  let closest = candidates[0];
-  let minDiff = verDiff(m.version, closest.version);
+  let picked = candidates[0];
+  let minDiff = verDiff(m.version, picked.version);
 
-  candidates.forEach((c, i) => {
-    if (i === 0) return;
-    const currentDiff = verDiff(c.version, m.version);
-    if (diffLt(currentDiff, minDiff)) {
-      minDiff = currentDiff;
-      closest = c;
+  for (let i = 1; i < candidates.length; i++) {
+    const c = candidates[i];
+
+    // if same version, use it
+    if (m.version.join('.') === c.version.join('.')) {
+      picked = c;
+      break;
     }
-  });
 
-  return museShared.modules[closest.museId];
+    // Ensure picked version is grater than target version
+    if (gt(picked.version, m.version) && lt(c.version, m.version)) continue;
+
+    // The version diff between the current version and the target version
+    const currentDiff = verDiff(c.version, m.version);
+
+    if ((lt(picked.version, m.version) && gt(c.version, m.version)) || lt(currentDiff, minDiff)) {
+      minDiff = currentDiff;
+      picked = c;
+    }
+  }
+
+  return museShared.modules[picked.museId];
 }
 
 module.exports = findMuseModule;
