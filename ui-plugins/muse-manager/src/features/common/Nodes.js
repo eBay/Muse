c915838bddb3c3a2ebd2edb05d497ccd81ded9aa
Nate Wang <pwang7@ebay.com>
2023-09-24 09:14:19 +0800

    Refactor plugin status.

--- ui-plugins/muse-manager/src/features/common/Nodes.js
+++ ui-plugins/muse-manager/src/features/common/Nodes.js
@@ -4,7 +4,18 @@ import { extendArray } from '@ebay/muse-lib-antd/src/utils';
  * A component that renders a list of nodes with ability to extend the list.
  * @param {Array} nodes - The list of nodes to render.
  */
-export default function Nodes({ nodes, extName, baseExt }) {
-  extendArray(nodes, 'nodes', extName, baseExt);
-  return nodes.map((n) => (n.render ? n.render() : n.node)).filter(Boolean);
+export default function Nodes({ items, extName = 'items', extBase, extArgs }) {
+  console.log(items, extName, extBase, extArgs);
+  items = items.filter(Boolean);
+  extendArray(items, extName, extBase, extArgs);
+  const nodes = [];
+  items.forEach((n) => {
+    let node;
+    if (n.render) node = n.render();
+    else if (n.node) node = n.node;
+    else if (n.component) node = <n.component key={n.key} {...n.props} />;
+    nodes.push(node);
+  });
+
+  return nodes;
 }
--- ui-plugins/muse-manager/src/features/pm/PluginStatus.js
+++ ui-plugins/muse-manager/src/features/pm/PluginStatus.js
@@ -3,45 +3,48 @@ import _ from 'lodash';
 import { Tag } from 'antd';
 import { Loading3QuartersOutlined } from '@ant-design/icons';
 import NiceModal from '@ebay/nice-modal-react';
-import jsPlugin from 'js-plugin';
+import Nodes from '../common/Nodes';
 
 function PluginStatus({ plugin, app }) {
   const { data: requests } = usePollingMuseData('muse.requests', { interval: 10000 });
   const onTagClick = (request, status) => {
     NiceModal.show('muse-manager.request-detail-modal', { request, status });
   };
-  const defaulTags =
-    requests?.map((req) => {
-      if (req?.payload?.pluginName === plugin.name) {
-        return req.statuses?.map((s) => {
-          const color = {
-            failure: 'error',
-            success: 'success',
-            pending: 'processing',
-          }[s.state];
+  const tags = [];
+  requests
+    ?.filter((req) => req?.payload?.pluginName === plugin.name)
+    .forEach((req) => {
+      req.statuses?.forEach((s, i) => {
+        const color = {
+          failure: 'error',
+          success: 'success',
+          pending: 'processing',
+        }[s.state];
 
-          const icon = color === 'processing' ? <Loading3QuartersOutlined spin /> : null;
-          return (
-            <Tag
-              key={req.id + '_' + s.name}
-              icon={icon}
-              style={{ cursor: 'pointer' }}
-              color={color}
-              onClick={() => onTagClick(req, s)}
-            >
-              {s.message || s.name + ' ' + s.state}
-            </Tag>
-          );
+        const icon = color === 'processing' ? <Loading3QuartersOutlined spin /> : null;
+        tags.push({
+          key: `${req.id}_${s.name}`,
+          order: i * 10 + 10,
+          component: Tag,
+          props: {
+            icon,
+            style: { cursor: 'pointer' },
+            color,
+            onClick: () => onTagClick(req, s),
+            children: s.message || s.name + ' ' + s.state,
+          },
         });
-      }
-      return null;
-    }) || [];
+      });
+    });
 
-  const customizedTags =
-    jsPlugin.invoke('museManager.pluginStatus.getStatusTags', { requests, plugin, app }) || [];
-
-  const allTags = _.flatten([...defaulTags, ...customizedTags]).filter(Boolean);
-  jsPlugin.invoke('museManager.pluginStatus.processStatusTags', { allTags, requests, plugin, app });
-  return <div className="grid gap-1 justify-items-start">{allTags}</div>;
+  return (
+    <div className="grid gap-1 justify-items-start">
+      <Nodes
+        items={tags}
+        extBase="museManager.pm.pluginStatus"
+        extArgs={{ items: tags, plugin, app, requests }}
+      />
+    </div>
+  );
 }
 export default PluginStatus;
