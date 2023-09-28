38846589403cb94df7dd27574f03ecb8ec7516dd
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-09-28 17:31:09 +0800

    MUSE-367 clean code

--- ui-plugins/muse-lib-react/tests/Root.test.js
+++ ui-plugins/muse-lib-react/tests/Root.test.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import store from '../src/common/store';
+import { render, screen } from '@testing-library/react';
 import Root from '../src/Root';
 
 describe('Root', () => {
   it('Root has no error', () => {
-    const DumpContainer = () => <div className="container">{this.props.children}</div>;
-    const NotFoundComp = () => <div className="not-found">Not found</div>;
-    const routes = [{
-      childRoutes: [
-        { path: '/', component: DumpContainer, childRoutes: [{ path: 'abc' }] },
-        { path: '/root', autoIndexRoute: true },
-        { path: 'relative-path', name: 'Link Name' },
-        {
-          path: 'sub-links',
-          childRoutes: [
-            { path: 'sub-link' },
-          ],
-        },
-        { path: '*', component: NotFoundComp },
-      ],
-    }];
-
-    render(<Root store={store} routeConfig={routes} />);
-
+    render(<Root />);
+    // check that default homepage is rendered
+    expect(screen.getByText('Welcome to Muse!')).toBeTruthy();
   });
 });
