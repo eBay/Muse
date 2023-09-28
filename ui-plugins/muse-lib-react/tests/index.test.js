371b45d8db1d4459b764b0b8151e37d8c37f6cb0
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-09-28 17:42:08 +0800

    MUSE-367 add index test

--- ui-plugins/muse-lib-react/tests/index.test.js
+++ ui-plugins/muse-lib-react/tests/index.test.js
@@ -1,7 +1,22 @@
-// index.js should run without errors.
+import React from 'react';
+import { act, waitFor, screen } from '@testing-library/react';
+
 describe('index', () => {
-  it('index.js has no error', () => {
+  it('index.js has no error', async () => {
     document.body.innerHTML = '<div id="root"></div>';
+    window.MUSE_GLOBAL.appEntries = [];
     require('../src/index');
+    
+    act(() => {
+      window.MUSE_GLOBAL.appEntries[0].func();
+    });
+
+    await waitFor(
+      () =>
+        expect(screen.getByText('Welcome to Muse!')).toBeTruthy(),
+      {
+        timeout: 5000,
+      },
+    );
   });
 });
