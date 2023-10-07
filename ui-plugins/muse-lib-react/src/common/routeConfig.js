a66d7b463d1fbad7397e1546c4c03a398642e219
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-10-07 12:34:29 +0800

    MUSE-367 clean up

--- ui-plugins/muse-lib-react/src/common/routeConfig.js
+++ ui-plugins/muse-lib-react/src/common/routeConfig.js
@@ -5,12 +5,11 @@ import { PageNotFound } from '../features/common';
 import homeRoute from '../features/home/route';
 import commonRoute from '../features/common/route';
 import _ from 'lodash';
-import userRoute from '../features/user/route';
 import subAppRoute from '../features/sub-app/route';
 
 // NOTE: DO NOT CHANGE the 'childRoutes' name and the declaration pattern.
 // This is used for Rekit cmds to register routes config for new features, and remove config when remove features, etc.
-const childRoutes = [homeRoute, commonRoute, userRoute, subAppRoute];
+const childRoutes = [homeRoute, commonRoute, subAppRoute];
 
 // Handle isIndex property of route config:
 //  Dupicate it and put it as the first route rule.
