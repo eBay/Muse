8e7bef17f9920c1f00cafa24d367ab5fe4a14efc
Nate Wang <pwang7@ebay.com>
2023-10-08 17:21:49 +0800

    Update comment

--- ui-plugins/muse-manager/src/features/common/Nodes.js
+++ ui-plugins/muse-manager/src/features/common/Nodes.js
@@ -2,7 +2,7 @@ import { extendArray } from '@ebay/muse-lib-antd/src/utils';
 
 /**
  * A component that renders a list of nodes with ability to extend the list.
- * @param {Array} nodes - The list of nodes to render.
+ * @param {Array} items - The list of nodes to render.
  */
 export default function Nodes({ items = [], extName = 'items', extBase, extArgs }) {
   items = items.filter(Boolean);
