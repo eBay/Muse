9be68db52ee2aefb191677390ea26c0b9e4202f7
Nate Wang <pwang7@ebay.com>
2023-10-09 19:06:36 +0800

    Adding more assets.

---
+++ ui-plugins/muse-lib-react/src/features/common/Nodes.js
@@ -0,0 +1,20 @@
+import { extendArray } from './utils';
+
+/**
+ * A component that renders a list of nodes with ability to extend the list.
+ * @param {Array} items - The list of nodes to render.
+ */
+export default function Nodes({ items = [], extName = 'items', extBase, extArgs }) {
+  items = items.filter(Boolean);
+  extendArray(items, extName, extBase, extArgs);
+  const nodes = [];
+  items.forEach(n => {
+    let node;
+    if (n.render) node = n.render();
+    else if (n.content) node = n.content;
+    else if (n.component) node = <n.component key={n.key} {...n.props} />;
+    nodes.push(node);
+  });
+
+  return nodes;
+}
