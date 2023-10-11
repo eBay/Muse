6943d2ff2ee1347f4d2a4d451060a234ece0ff01
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-10-11 16:07:43 +0800

    MUSE-367 Highligher UT

--- ui-plugins/muse-lib-antd/src/features/common/Highlighter.js
+++ ui-plugins/muse-lib-antd/src/features/common/Highlighter.js
@@ -5,7 +5,7 @@ import ReactHighlighter from 'react-highlight-words';
 export default function Highlighter({ search, text }) {
   return (
     <ReactHighlighter
-      searchWords={[search]}
+      searchWords={search}
       textToHighlight={text}
       highlightClassName="muse-antd_common-highlighter-span"
       highlightStyle={{backgroundColor: 'yellow'}}
