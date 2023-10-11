a0afb7b2849af8c4c1f087fc4714de96e6d6f8d8
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-10-11 10:49:49 +0800

    MUSE-367 BlockView UT

--- ui-plugins/muse-lib-antd/tests/features/common/BlockView.test.js
+++ ui-plugins/muse-lib-antd/tests/features/common/BlockView.test.js
@@ -3,9 +3,28 @@ import { render, screen } from '@testing-library/react';
 import { BlockView } from '../../../src/features/common';
 
 describe('common/BlockView', () => {
-  it('renders BlockView', () => {
+  it('renders BlockView with emails', () => {
     render(<BlockView value={['Olive', 'Allen', 'Gerry', 'Cindy', 'Ezra']} openEmail={true} />);
+    expect(screen.getByRole('link', { name: 'Olive' }).getAttribute('href')).toBe('mailto:Olive@ebay.com')
+    expect(screen.getByRole('link', { name: 'Ezra' }).getAttribute('href')).toBe('mailto:Ezra@ebay.com')
+  });
+
+  it('renders BlockView without emails', () => {
+    render(<BlockView value={['Olive', 'Allen', 'Gerry', 'Cindy', 'Ezra']} openEmail={false} />);
+    expect(screen.queryAllByRole('link')).toEqual([]);
     expect(screen.getByText('Olive')).toBeTruthy();
     expect(screen.getByText('Ezra')).toBeTruthy();
+  });
+
+  it('renders BlockView with non-string values', () => {
+    render(<BlockView value={[{ id: 1 }]} openEmail={false} />);
+    expect(screen.queryAllByRole('link')).toEqual([]);
+    expect(screen.getByText('[object]')).toBeTruthy();    
+  });
+
+  it('renders BlockView with non-array value', () => {
+    render(<BlockView value={'Olive'} openEmail={false} />);
+    expect(screen.queryAllByRole('link')).toEqual([]);
+    expect(screen.getByText('Olive')).toBeTruthy();    
   });
 });
