ab00be553923a1ef35f365c72d440129e9de8c06
Miguel Cubells Baeza <mcubellsbaeza@ebay.com>
2023-10-12 16:15:43 +0800

    MUSE-367 TableBar UT

--- ui-plugins/muse-lib-antd/tests/features/common/TableBar.test.js
+++ ui-plugins/muse-lib-antd/tests/features/common/TableBar.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { TableBar } from '../../../src/features/common';
 
 describe('common/TableBar', () => {
-  it('renders TableBar', () => {
-    render(<TableBar onSearch={() => {}} placeholder={"Search something"}>Component to search on</TableBar>);
+  it('renders TableBar', async () => {
+    console.log = jest.fn();
+    render(<TableBar 
+      onSearch={(v) => { 
+        console.log(v)
+      }} 
+      placeholder={"Search something"}>
+        <span>Component to search on</span>
+      </TableBar>);
     
+    expect(screen.getByPlaceholderText('Search something')).toBeTruthy();
+    expect(screen.getByText('Component to search on')).toBeTruthy();
+    const textbox = screen.getByRole('textbox');
+    userEvent.clear(textbox);
+    userEvent.type(textbox, 'hello');
+    await waitFor(() => expect(console.log).toHaveBeenCalledWith('hello'), { timeout: 5000});
   });
 });
