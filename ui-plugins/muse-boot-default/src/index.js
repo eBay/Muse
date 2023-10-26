415a5dd79992dc11eaaed7f33516c2aff48c58eb
Lin Gao <gling@ebay.com>
2023-10-26 17:45:19 +0800

    fix allowlist did not work on plugin level.

--- ui-plugins/muse-boot-default/src/index.js
+++ ui-plugins/muse-boot-default/src/index.js
@@ -242,7 +242,7 @@ async function start() {
 
   // Load normal and lib plugins
   const bundleDir = isDev ? 'dev' : isE2eTest ? 'test' : 'dist';
-  const pluginsToLoad = plugins
+  const pluginsToLoad = window.MUSE_GLOBAL.plugins
     .filter((p) => p.type !== 'boot' && p.type !== 'init')
     .map((p) => {
       return {
