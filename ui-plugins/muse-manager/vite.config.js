c8e84ea05737bd138ce248755996b6cdbd2bfbf7
Nate Wang <pwang7@ebay.com>
2024-01-12 08:56:02 +0800

    Update vite.config.js

--- ui-plugins/muse-manager/vite.config.js
+++ ui-plugins/muse-manager/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs-extra';
 import museEsbuildPlugin from '@ebay/muse-vite-plugin/lib/museEsbuildPlugin';
 import museVitePlugin from '@ebay/muse-vite-plugin/lib/museVitePlugin';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
@@ -12,7 +11,7 @@ export default defineConfig(({ command, mode }) => {
     },
     plugins: [react(), museVitePlugin()],
     server: {
-      // if port is specified in env, use it strictly
+      // If port is specified in env, use it strictly
       // otherwise vite uses 5173 as default port and will try to use other ports if 5173 is occupied
       port: process.env.PORT,
       strictPort: true,
