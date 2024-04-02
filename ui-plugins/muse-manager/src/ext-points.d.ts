9d3df596f925f2701b7d8e79f837a0085167f7f2
Lin Gao <gling@ebay.com>
2024-04-02 21:51:12 +0800

    Refine ext-points.d.ts file

--- ui-plugins/muse-manager/src/ext-points.d.ts
+++ ui-plugins/muse-manager/src/ext-points.d.ts
@@ -1,48 +1,191 @@
 import type { TabsProps } from 'antd';
+import { ComponentType, ReactNode } from 'react';
+import NiceFormMeta, { NiceFormFieldType } from '@ebay/nice-form-react/lib/esm/NiceFormMeta';
+import type { ColumnsType } from 'antd/es/table';
+import type { PopconfirmProps } from 'antd/es/popconfirm';
+import type { FormInstance } from 'antd/es/form';
 
-type NodeType = {
-  order?: number,
-  node?: any,
-  component?: any,
+// App type for Muse V2
+export type MuseV2App = {
+  name: string;
+  createdBy: string;
+  createdAt: string;
+  title: string;
+  owners: string[];
+  envs?: {
+    [key: string]: MuseV2Env;
+  };
+  iconId?: number;
+  config?: {
+    entry?: string;
+    theme?: 'dark' | 'light' | 'custom'
+    noSSO?: boolean;
+    allowlist?: string[];
+    csp?: string;
+  };
+  pluginConfig?: {
+    [key: string]: MuseV2PluginConfig;
+  };
+  pluginVariables?: Record<string, any>;
+  variables?: Record<string, any>;
+  description?: string;
+  [key: string]: any;
+}
+
+// Environment type for Muse V2
+export type MuseV2Env = {
+  name: string;
+  createdBy: string;
+  createdAt: string;
+  url: string | string[];
+  c2sProxy: boolean;
+  e2eTestMode: string;
+  pluginVariables?: Record<string, any>;
+  variables?: Record<string, any>;
+  config?: {
+    [key: string]: any;
+  }
+  [key: string]: any;
+}
+
+// Plugin config type for Muse V2
+export type MuseV2PluginConfig = {
+  core?: boolean;
+  allowlist?: string[];
+  [key: string]: any;
 };
 
+// Plugin type for Muse V2
+export type MuseV2Plugin = {
+  name: string;
+  pluginName?: string; // compatible with Muse V1
+  app?: string; // owner app
+  createdBy: string;
+  createdAt: string;
+  type: 'normal' | 'lib' | 'boot' | 'init',
+  owners: string[];
+  repo: string;
+  repoPath?: string; // for monorepo
+  ut?: boolean;
+  description?: string;
+  config?: Record<string, any>; // deprecated ???
+  variables?: Record<string, any>; // deprecated
+  [key: string]: any;
+};
+
+// Release type for Muse V2
+export type MuseV2Release = {
+  pluginName: string;
+  version: string;
+  branch: string;
+  sha: string;
+  createdBy: string;
+  createdAt: string;
+  description: string;
+  [key: string]: any;
+};
+
+// Request type for Muse V2
+export type MuseV2Request = {
+  id: string;
+  type: string;
+  autoComplete: boolean;
+  createdBy: string;
+  createdAt: string;
+  description: string;
+  payload: Record<string, any>;
+  statuses: Record<string, any>[];
+  [key: string]: any;
+}
+
+// Nodes component ext points
+export type NodeType<C extends ComponentType<any> = ComponentType<any>> = {
+  key?: string;
+  order?: number,
+  node?: ReactNode;
+  component?: C,
+  render?: (args: any) => ReactNode,
+  props?: React.ComponentProps<C>,
+};
 export type getNodes = (args: any) => NodeType | NodeType[];
-export type getFields = (args: any) => any | any[];
-export type getScopes = (args: any) => any | any[];
-export type getColumns = (args: any) => any | any[];
-export type getActions = (args: any) => any | any[];
-export type getItems = (args: any) => any | any[];
+export interface NodesExtPoints<Context> {
+  preProcessNodes?: (args: Context) => void;
+  getNodes?: (args: Context) => NodeType | NodeType[];
+  processNodes?: (args: Context) => void;
+  postProcessNodes?: (args: Context) => void;
+}
 
-export type processDropdownItems = (args: any) => void;
 
-export interface ListAbilities {
+// Ext points on a list data
+export type getItems<T> = (args: any) => T | T[];
+export interface ListDefaultExtPoints<T> {
   preProcessItems?: (args: any) => void;
-  getItems?: getItems;
+  getItems?: getItems<T>;
   processItems?: (args: any) => void;
   postProcessItems?: (args: any) => void;
 }
 
-export interface TableColumnsAbilities {
-  preProcessColumns?: (args: any) => void;
-  getColumns?: getColumns;
-  processColumns?: (args: any) => void;
-  postProcessColumns?: (args: any) => void;
+// Table columns ext points
+export type getColumns<RecordType> = (args: any) => ColumnsType<RecordType> | ColumnsType<RecordType>[];
+export interface TableColumnsExtPoints<RecordType, Context> {
+  preProcessColumns?: (args: Context) => void;
+  getColumns?: (args: Context) => ColumnsType<RecordType> | ColumnsType<RecordType>[];
+  processColumns?: (args: Context) => void;
+  postProcessColumns?: (args: Context) => void;
 }
 
-
-export interface NiceFormComponent {
-  getWatchingFields?: () => string[];
-  getFields?: getFields,
-  preProcessMeta?: (args: any) => void,
-  postProcessMeta?: (args: any) => void,
-  processMeta?: (args: any) => void,
+// NiceForm ext points
+export type getFields = (args: any) => NiceFormFieldType | NiceFormFieldType[];
+export interface NiceFormExtPoints<Context> {
+  getWatchingFields?: (args: Context) => string[];
+  getFields?: (args: Context) => NiceFormFieldType | NiceFormFieldType[],
+  preProcessMeta?: (args: Context) => void,
+  postProcessMeta?: (args: Context) => void,
+  processMeta?: (args: Context) => void,
 }
 
-export interface NiceModalComponent {
-  form?: NiceFormComponent;
-  body?: ListAbilities;
-  footer?: ListAbilities;
+// NiceModal ext points
+export interface NiceModalComponent<T, NiceFormContext> {
+  form?: NiceFormExtPoints<NiceFormContext>;
+  body?: ListDefaultExtPoints<T>;
+  footer?: ListDefaultExtPoints<T>;
 }
+
+// Actions ext points
+export type ActionType = {
+  key: string;
+  label: ReactNode;
+  highlight?: boolean;
+  icon?: string | React.JSX.Element;
+  onClick?: (args: any) => void;
+  order?: number;
+  disabled?: boolean;
+  disabledText?: string;
+  render?: (args: any) => ReactNode;
+  confirm?: PopconfirmProps & {
+    key: string;
+  }
+  [key: string]: any;
+};
+export type getActions = (args: any) => ActionType | ActionType[];
+export interface ActionsExtPoints<Context> {
+  preProcessActions?: (args: Context) => void;
+  getActions?: getActions;
+  processActions?: (args: Context) => void;
+  postProcessActions?: (args: Context) => void;
+}
+
+// Table bar ext points
+type ScopeType = {
+  key: string;
+  label: ReactNode;
+  order: number;
+  onClick: (args: any) => void;
+}
+export type getScopes = (args: any) => ScopeType | ScopeType[];
+export type processDropdownItems = (args: any) => void;
+
+
 
 export interface ListBar {
   getScopes?: getScopes;
@@ -64,43 +207,113 @@ export interface AppPage {
   getTabs?: (args: any) => TabsProps['items'];
 }
 
+type NiceFormContext = {
+  meta: NiceFormMeta;
+  form?: FormInstance;
+  app?: MuseV2App;
+  [key: string]: any;
+}
+type EnvironmentTableContext = {
+  app?: MuseV2App;
+  columns?: ColumnsType<Record<string, any>>;
+}
 export interface AM {
   appPage?: AMAppPage;
   appOverview?: AppOverview;
-  createAppForm?: NiceFormComponent;
-  appBasicInfo?: NiceFormComponent;
-  editAppForm?: NiceFormComponent;
+  createAppForm?: NiceFormExtPoints<NiceFormContext>;
+  appBasicInfo?: NiceFormExtPoints<NiceFormContext>;
+  editAppForm?: NiceFormExtPoints<NiceFormContext>;
+  environments?: TableColumnsExtPoints<Record<string, any>, EnvironmentTableContext>;
 }
 
+type PluginActionContext<RecordType> = {
+  app?: MuseV2App;
+  columns?: ColumnsType<RecordType>;
+  plugins?: MuseV2Plugin[];
+  searchValue?: string;
+  latestReleases?: MuseV2Release[];
+  [key: string]: any;
+};
+type EnvFilter = {
+  key: string;
+  label: ReactNode;
+};
+type PluginBadgeContext = {
+  app?: MuseV2App;
+  plugin?: MuseV2Plugin;
+  [key: string]: any;
+};
+type PluginListTableContext = {
+  app?: MuseV2App,
+  columns?: ColumnsType<any>,
+  plugins?: MuseV2Plugin[],
+  searchValue?: string,
+  latestReleases?: { [key: string]: MuseV2Release },
+  [key: string]: any;
+}
+export interface PluginListExtPoints<RecordType> extends
+  TableColumnsExtPoints<RecordType, PluginListTableContext>,
+  ActionsExtPoints<PluginActionContext<RecordType>> {
+  pluginBadges?: NodesExtPoints<PluginBadgeContext>;
+  getEnvFilters?: () => EnvFilter | EnvFilter[];
+  getEnvFilterFns?: ({ filterKey, app, envName }: { filterKey: string, app: MuseV2App, envName: string }) => undefined | Function[];
+  getScopeFilterFns?: ({ scope }: { scope: string }) => null | Function;
+}
+
+type ReleaseActionContext = {
+  items?: ActionType[];
+  app?: MuseV2App;
+  plugin?: MuseV2Plugin;
+  ability?: any;
+  [key: string]: any;
+};
+type ReleaseNodeContext = {
+  plugin?: MuseV2Plugin;
+  releases?: MuseV2Release[];
+  [key: string]: any;
+}
+type ExpandRowNodeContext = {
+  release?: MuseV2Release;
+};
+type ReleaseTableContext = {
+  plugin?: MuseV2Plugin, app?: MuseV2App, releases?: MuseV2Release[], [key: string]: any;
+};
+export interface ReleaseListExtPoints<RecordType> extends
+  TableColumnsExtPoints<RecordType, ReleaseTableContext>,
+  ActionsExtPoints<ReleaseActionContext>,
+  NodesExtPoints<ReleaseNodeContext> {
+  expandRow?: NodesExtPoints<ExpandRowNodeContext>
+}
+
+export type PluginStatusContext = {
+  plugin?: MuseV2Plugin;
+  app?: MuseV2App;
+  request?: MuseV2Request;
+};
+
+export type PluginConfigFormExtPoints = NiceFormExtPoints<NiceFormContext> & {
+  processValues?: (context: { values: Record<string, any>, form: FormInstance }) => void;
+};
+
+
 export type PM = {
-  pluginInfoForm?: NiceFormComponent;
-  pluginList?: TableColumnsAbilities & {
-    getPluginBadges?: getNodes;
-    preProcessPluginActions?: (args: any) => void;
-    getPluginActions?: getActions;
-    processPluginActions?: (args: any) => void;
-    postProcessPluginActions?: (args: any) => void;
-  };
-  releaseList?: TableColumnsAbilities & ListAbilities & {
-    processReleaseActions: (args: any) => void;
-    getExpandNodes: (args: any) => NodeType | NodeType[];
-  };
-  deployPluginModal?: NiceModalComponent;
-  undeployPluginModal?: NiceModalComponent;
-  groupDeployModal?: NiceModalComponent;
-  releaseInfoModal?: NiceModalComponent;
-  pluginConfigForm?: NiceFormComponent;
+  pluginInfoForm?: NiceFormExtPoints<NiceFormContext>;
+  pluginList?: PluginListExtPoints<any>;
+  releaseList?: ReleaseListExtPoints<any>;
+  deployPluginModal?: NiceModalComponent<any, NiceFormContext>;
+  undeployPluginModal?: NiceModalComponent<any, NiceFormContext>;
+  groupDeployModal?: NiceModalComponent<any, NiceFormContext>;
+  releaseInfoModal?: NiceModalComponent<any, NiceFormContext>;
+  pluginConfigForm?: NiceFormExtPoints<NiceFormContext>;
   pluginStatus?: {
-    relatedToPlugin?: (args: any) => void | boolean;
-    processRequest?: (args: any) => void;
+    relatedToPlugin?: (args: PluginStatusContext) => void | boolean;
+    processRequest?: (args: PluginStatusContext) => void;
   };
-  createPluginForm?: NiceFormComponent & {
-    processValues?: (args: any) => void;
-  }
+  createPluginForm?: PluginConfigFormExtPoints
 };
 
 export interface Req {
-  requestDetailModal?: NiceModalComponent & {
+  requestDetailModal?: NiceModalComponent<any, NiceFormContext> & {
     processModalProps?: (args: any) => void;
   }
 }
@@ -111,10 +324,7 @@ export default interface MuseManagerExtPoints {
   am?: AM;
   pm?: PM;
   req?: Req;
-  editEnvForm?: NiceFormComponent;
-  addEnvForm?: NiceFormComponent;
+  editEnvForm?: NiceFormExtPoints<NiceFormContext>;
+  addEnvForm?: NiceFormExtPoints<NiceFormContext>;
   pluginListBar?: pluginListBar;
-  getEnvironmentsColumns?: getColumns;
-  processEnvironmentsColumns?: (args: any) => void;
-  buildPluginForm?: NiceFormComponent;
 }
