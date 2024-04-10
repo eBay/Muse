c4cea94ec3e2ab8c65f4afbb4c2a8db9b21a44a4
Lin Gao <gling@ebay.com>
2024-04-10 14:00:56 +0800

    support specifying version of ext-points.d.ts

--- workspace/packages/muse-dev-utils/scripts/syncExtPointsTypes.js
+++ workspace/packages/muse-dev-utils/scripts/syncExtPointsTypes.js
@@ -4,8 +4,18 @@ const path = require('path');
 const mkdirp = require('mkdirp');
 const { utils, storage, data: museData } = require('@ebay/muse-core');
 
+/**
+ * @description Sync ext-points.d.ts files for the specified plugins or all deployed plugins on the app.
+ * The ext-points.d.ts files are downloaded from the storage service and saved to the local file system.
+ * The files are saved in the .ext-points-types folder in the root of the project.
+ * The file name is the pluginId.d.ts.
+ * The file is overwritten if it already exists.
+ * The version number is added as a comment in the first line of the file.
+ * Usage:
+ *  pnpm sync-ext-points-types [pluginName@version] [pluginName@version] ...
+ */
 module.exports = async function syncExtPointsTypeFiles() {
-  // Analyze the plugins's version and get the corresponding ext-point-type
+  // Analyze the plugins's version and get the corresponding ext-points.d.ts file
   const pkg = fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf-8');
   const pkgJson = JSON.parse(pkg);
   const app = pkgJson.muse?.devConfig?.app;
@@ -17,10 +27,10 @@ module.exports = async function syncExtPointsTypeFiles() {
 
   const pluginNameArgs = process.argv.slice(3);
   if (pluginNameArgs.length === 0) {
-    // no plugin specified, install for all deployed plugins
+    // no plugin specified, download all deployed plugins
     console.log(
       chalk.cyan(
-        `No plugin specified. Will install ext-point-type.d.ts for all deployed plugins on app "${app}" in env "${env}".`,
+        `No plugin specified. Will download each ext-points.d.ts for all deployed plugins on app "${app}" in env "${env}".`,
       ),
     );
     const appRegistryData = await museData.get(`muse.app.${app}`);
@@ -43,26 +53,26 @@ module.exports = async function syncExtPointsTypeFiles() {
     for (const plugin of deployedPlugins) {
       const pluginName = plugin.name;
       const pluginVersion = plugin.version;
-      await downloadExtPointsTypeFile(pluginName, pluginVersion);
+      await downloadExtPointsTypesFile(pluginName, pluginVersion);
     }
   } else {
-    // plugin specified, install for the specified plugins
-    console.log(
-      chalk.cyan('Installing ext-point-type.d.ts for plugins: ', pluginNameArgs.join(', ')),
-    );
+    // plugin specified, dowload for the specified plugins
+    console.log(chalk.cyan('Downloading ext-points.d.ts for plugin: ', pluginNameArgs.join(', ')));
     const pluginsLatestRelease = await museData.get(`muse.plugins.latest-releases`);
-    if (!pluginsLatestRelease) {
-      console.log(chalk.red('Error: pluginsLatestRelease is empty.'));
-      return;
-    }
-
-    for (const pluginName of pluginNameArgs) {
-      const { version: latestVersion } = pluginsLatestRelease[pluginName] || {};
-      if (!latestVersion) {
-        console.log(chalk.yellow(`Warn: plugin "${pluginName}" does not have a latest version.`));
+    const regex = /^(@?[^@]+)@(.+)$/; // match pluginName@version
+    for (const plugin of pluginNameArgs) {
+      const match = plugin.match(regex);
+      const pluginName = match?.[1] || plugin;
+      const version = match?.[2] || pluginsLatestRelease?.[plugin];
+      if (!version) {
+        console.log(
+          chalk.yellow(
+            `Warning: The plugin "${pluginName}" could not locate a version number. Skipping this plugin`,
+          ),
+        );
         continue;
       }
-      await downloadExtPointsTypeFile(pluginName, latestVersion);
+      await downloadExtPointsTypesFile(pluginName, version);
     }
   }
 };
@@ -73,7 +83,7 @@ module.exports = async function syncExtPointsTypeFiles() {
  * @param {string} pluginVersion
  * @returns
  */
-async function downloadExtPointsTypeFile(pluginName, pluginVersion) {
+async function downloadExtPointsTypesFile(pluginName, pluginVersion) {
   let res;
   try {
     res = await storage.assets.get(
@@ -81,13 +91,13 @@ async function downloadExtPointsTypeFile(pluginName, pluginVersion) {
     );
   } catch (e) {
     console.log(
-      chalk.red(`Error: failed to get file ext-point-type.d.ts for plugin "${pluginName}".`),
+      chalk.red(`Error: failed to download file ext-point-type.d.ts for plugin "${pluginName}".`),
     );
     return;
   }
   if (!res) {
     console.log(
-      chalk.yellow(`Warn: file ext-point-type.d.ts for plugin "${pluginName}" does not exist.`),
+      chalk.yellow(`Warn: file ext-points.d.ts for plugin "${pluginName}" does not exist.`),
     );
     return;
   }
