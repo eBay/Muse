349794c6434139b9867395182c79a95d4eb0961c
Nate Wang <pwang7@ebay.com>
2024-04-27 13:49:01 +0800

    Update vite config

--- ui-plugins/muse-runner-ui/vite.config.js
+++ ui-plugins/muse-runner-ui/vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import museEsbuildPlugin from '@ebay/muse-vite-plugin/lib/museEsbuildPlugin';
-import museVitePlugin from '@ebay/muse-vite-plugin/lib/museVitePlugin';
+import museVitePlugin from '@ebay/muse-vite-plugin';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), museVitePlugin()],
-    server: {
-      // If port is specified in env, use it strictly
-      // otherwise vite uses 5173 as default port and will try to use other ports if 5173 is occupied
-      port: process.env.PORT,
-      // strictPort: true,
-    },
-    optimizeDeps: {
-      needsInterop: [],
-      esbuildOptions: {
-        plugins: [museEsbuildPlugin()],
-      },
-    },
   };
 });
