c4663406d63a2f057dd3fbdcce07b5d471d0e09e
Nate Wang <pwang7@ebay.com>
2024-04-29 16:33:14 +0800

    Added ebay muse vite plugin

--- ui-plugins/muse-manager/vite.config.mjs
+++ ui-plugins/muse-manager/vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import museVitePlugin from '@ebay/muse-vite-plugin/lib/museVitePlugin';
+import museVitePlugin from '@ebay/muse-vite-plugin';
+import museVitePluginEbay from '@ebay/muse-vite-plugin-ebay';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -9,6 +10,6 @@ export default defineConfig(({ mode }) => {
       'process.env.REACT_APP_MUSE_API_ENDPOINT': JSON.stringify(env.REACT_APP_MUSE_API_ENDPOINT),
     },
 
-    plugins: [react(), museVitePlugin()],
+    plugins: [react(), museVitePlugin(), museVitePluginEbay()],
   };
 });
