84c0b946b69a355b4ff6c25826a099febdc82935
Nate Wang <pwang7@ebay.com>
2024-05-07 08:50:34 +0800

    up muse modules in webpack plugin

--- workspace/packages/muse-webpack-plugin/lib/index.js
+++ workspace/packages/muse-webpack-plugin/lib/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 module.paths.splice(0, 2, path.join(process.cwd(), 'node_modules'));
+require('@ebay/muse-modules').config.matchVersion = 'major';
 module.exports = {
   MusePlugin: require('./MusePlugin'),
   MuseReferencePlugin: require('./MuseReferencePlugin'),
