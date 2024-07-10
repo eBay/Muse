b1d63420a81e11f9a7c4757864c27cb1299b398a
Wang, Nate <pwang7@ebay.com>
2024-07-10 14:45:24 +0800

    refine css asset

--- ui-plugins/muse-manager/vite.config.js
+++ ui-plugins/muse-manager/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import museVitePlugin from '@ebay/muse-vite-plugin';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -12,6 +11,6 @@ export default defineConfig(({ mode }) => {
     server: {
       host: 'local.cloud.ebay.com',
     },
-    plugins: [react(), museVitePlugin(), cssInjectedByJsPlugin()],
+    plugins: [react(), museVitePlugin()],
   };
 });
