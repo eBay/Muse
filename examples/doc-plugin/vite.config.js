0136aec1126a9c0288ade8cf7992df0d8d56daab
LeChang97 <lchang1@ebay.com>
2025-10-28 15:39:28 +0800

    change doc-plugin dependencies

--- examples/doc-plugin/vite.config.js
+++ examples/doc-plugin/vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import museVitePlugin from '@ebay/muse-vite-plugin';
-
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
+    define: {
+      'process.env.REACT_APP_MUSE_API_ENDPOINT': JSON.stringify(env.REACT_APP_MUSE_API_ENDPOINT),
+    },
+    server: {
+      host: 'local.cloud.ebay.com',
+    },
     plugins: [react(), museVitePlugin()],
   };
 });
