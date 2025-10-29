72f74090dfffe7a1d4884937dff04e6c00629e37
Chang, Le(CCC) <lchang1@ebay.com>
2025-10-29 10:47:36 +0800

    change to plugin-react-swc

--- examples/doc-plugin/vite.config.js
+++ examples/doc-plugin/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import museVitePlugin from '@ebay/muse-vite-plugin';
 export default defineConfig(({ mode }) => {
   return {
