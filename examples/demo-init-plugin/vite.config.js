5f4add5b9b082a8aaa2cd71083d9e4ed9cf40624
Chang, Le(CCC) <lchang1@ebay.com>
2025-11-04 09:57:58 +0800

    modify vite.vonfig.js

--- examples/demo-init-plugin/vite.config.js
+++ examples/demo-init-plugin/vite.config.js
@@ -1,19 +1,37 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+/*  global process  */
 
-export default defineConfig(({ mode }) => {
+import fs from 'fs';
+import { defineConfig } from 'vite';
+
+const sslCrtFile = process.env.SSL_CRT_FILE;
+const sslKeyFile = process.env.SSL_KEY_FILE;
+
+export default defineConfig(() => {
+  const isHTTPS = process.env.HTTPS === 'true';
+  const port = process.env.PORT;
+  const host = process.env.MUSE_LOCAL_HOST_NAME || 'localhost';
+
   return {
-    build: {
-      outDir: path.resolve(__dirname, 'build/dist'),
-      rollupOptions: {
-        input: 'src/index.js',
-        output: {
-          entryFileNames: 'main.js',
-        },
+    server: {
+      origin: port ? `${isHTTPS ? 'https' : 'http'}://${host}:${port}` : undefined,
+      port,
+      host,
+      strictPort: !!port,
+      https: process.env.HTTPS === 'true' && {
+        cert: fs.readFileSync(sslCrtFile),
+        key: fs.readFileSync(sslKeyFile),
       },
     },
-    server: {
-      port: process.env.PORT,
+    build: {
+      sourcemap: true,
+      outDir: 'build/dist',
+      rollupOptions: {
+        input: 'src/main.js',
+        output: {
+          entryFileNames: 'main.js',
+          format: 'iife',
+        },
+      },
     },
   };
 });
