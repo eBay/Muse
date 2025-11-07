470bf87b1ac53d3db5f0ef79a2868251b9a11d47
Wang, Nate <pwang7@ebay.com>
2025-11-07 16:44:23 +0800

    UI plugins release flow

--- muse-ci-tools/scripts/buildAndPublishUiPlugins.js
+++ muse-ci-tools/scripts/buildAndPublishUiPlugins.js
@@ -1,3 +1,7 @@
+import debug from 'debug';
 import buildAndPublishUiPlugins from '../src/setup/buildAndPublishUiPlugins.js';
+
+console.log('using debug mode');
+debug.enable('muse:*');
 
 await buildAndPublishUiPlugins();
