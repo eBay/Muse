9e39d5b5fdc364bcc8970e77988154f0c6abe54c
Lin Gao <gling@ebay.com>
2026-07-27 16:09:20 +0800

    fix: update drawer size in ReleasesDrawer component

--- ui-plugins/muse-manager/src/features/pm/ReleasesDrawer.jsx
+++ ui-plugins/muse-manager/src/features/pm/ReleasesDrawer.jsx
@@ -214,7 +214,7 @@ const ReleasesDrawer = NiceModal.create(({ plugin, app }) => {
     },
   ];
   return (
-    <Drawer {...antdDrawerV5(modal)} title={`Releases of ${plugin.name}`} size="large">
+    <Drawer {...antdDrawerV5(modal)} title={`Releases of ${plugin.name}`} size={1200}>
       <RequestStatus loading={isLoading} error={error} loadingMode="skeleton" />
       {!isLoading && (
         <Nodes
